b3ceeb08e74785711ab224ef997b256e8f1a7172
Pratyaksh <pratyaksh.chandra2898@gmail.com>
2022-03-20 17:42:55 +0530

    Project initialization and removed the boiler plate

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+        <Text>Hexa Keepr</Text>    
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+});
+
+export default App;
