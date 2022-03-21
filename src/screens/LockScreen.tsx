611facfc5e2300806c564df8676c357ad48b2823
Pratyaksh <pratyaksh.chandra2898@gmail.com>
2022-03-22 00:08:27 +0530

    Added expo module support and local-auth for biometrics

--- src/screens/LockScreen.tsx
+++ src/screens/LockScreen.tsx
@@ -1,10 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { StyleSheet, Text, View, Alert } from 'react-native';
+import React, { useEffect } from 'react';
+import * as LocalAuthentication from 'expo-local-authentication';
 
 const LockScreen = () => {
+  const [isBiometricSupported, setIsBiometricSupported] = React.useState(false);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const compatible = await LocalAuthentication.hasHardwareAsync();
+        setIsBiometricSupported(compatible);
+      } catch (err) {
+        console.log('error', err);
+      }
+    })();
+  });
   return (
     <View style={styles.screen}>
       <Text>LockScreen</Text>
+      <Text>
+        {' '}
+        {isBiometricSupported ? 'Your device is compatible with Biometrics' : 'Fuck you'}
+      </Text>
     </View>
   );
 };
