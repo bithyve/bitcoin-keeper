9b9cdbaae8ca24b9801e30e60d45409a74c37fde
Pratyaksh <pratyaksh.chandra2898@gmail.com>
2022-04-03 02:19:24 +0530

    add wallet bug

--- src/screens/AddWalletScreen.tsx
+++ src/screens/AddWalletScreen.tsx
@@ -193,14 +193,14 @@ const AddWalletScreen = () => {
   const dispatch = useDispatch();
 
   const addWallet = useCallback(() => {
-    // const newAccountShellInfo: newAccountsInfo = {
-    //   accountType: AccountType.CHECKING_ACCOUNT,
-    //   accountDetails: {
-    //     name: accountName,
-    //     description: accountDescription,
-    //   },
-    // };
-    // dispatch(addNewAccountShells([newAccountShellInfo]));
+    const newAccountShellInfo: newAccountsInfo = {
+      accountType: AccountType.CHECKING_ACCOUNT,
+      accountDetails: {
+        name: accountName,
+        description: accountDescription,
+      },
+    };
+    dispatch(addNewAccountShells([newAccountShellInfo]));
     setWalletDetails({
       name: accountName,
       description: accountDescription,
