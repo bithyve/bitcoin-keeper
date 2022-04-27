aeea8fc5eb9de838f444313890dc5c5a8e53dcf7
Pratyaksh <pratyaksh.chandra2898@gmail.com>
2022-04-28 03:30:19 +0530

    added colors related file

---
+++ src/theme/Colors.ts
@@ -0,0 +1,10 @@
+const Colors = {
+  white: 'rgb(250, 250, 250)',
+  black: 'rgb(0, 0, 0)',
+  primary: 'rgb(7, 62, 57)',
+  primaryBackgroundColor: 'rgb(253, 247, 240)',
+  secondaryBackgroundColor: 'rgb(236, 209, 182)',
+  loginScreenBackground: 'rgb(47, 47, 47)',
+  pinFiller: 'rgba(98, 197, 191, 1)',
+};
+export default Colors;
