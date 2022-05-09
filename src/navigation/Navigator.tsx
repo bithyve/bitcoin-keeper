e102145babc3c1e4d2c01cd16eac8d7b17c7aee7
Raheel1258 <rahilahmad1258@gmail.com>
2022-05-09 19:34:42 +0500

    add account screen

--- src/navigation/Navigator.tsx
+++ src/navigation/Navigator.tsx
@@ -5,9 +5,10 @@ import InheritanceScreen from 'src/screens/Inheritance/InheritanceScreen';
 import HomeScreen from 'src/screens/HomeScreen';
 import AddWallet from '../screens/AddWalletScreen';
 import TestingBottomsheet from 'src/screens/TestingBottomsheet';
-import QRscanner from 'src/screens/QRscannerScreen';
-import LoginScreen from 'src/screens/LoginScreen';
+import QRscannerScreen from 'src/screens/QRscannerScreen/QRScannerScreen';
+import LoginScreen from 'src/screens/LoginScreen/LoginScreen';
 import BackupScreen from 'src/screens/BackupScreen';
+import AccountDetailScreen from 'src/screens/AccountDetailScreen';
 
 const defaultTheme = {
   ...DefaultTheme,
@@ -22,11 +23,12 @@ const Navigator = () => {
   return (
     <NavigationContainer theme={defaultTheme}>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {/* <Stack.Screen name="Lock Screen" component={LoginScreen} /> */}
+        <Stack.Screen name="AccountDetailScreen" component={AccountDetailScreen} />
+        <Stack.Screen name="Lock Screen" component={LoginScreen} />
         <Stack.Screen name="Home" component={HomeScreen} options={{ gestureEnabled: false }} />
         <Stack.Screen name="Backup" component={BackupScreen} />
         <Stack.Screen name="Inheritance" component={InheritanceScreen} />
-        <Stack.Screen name="QRscanner" component={QRscanner} />
+        <Stack.Screen name="QRscanner" component={QRscannerScreen} />
         <Stack.Screen name="AddWallet Screen" component={AddWallet} />
         <Stack.Screen name="Test" component={TestingBottomsheet} />
       </Stack.Navigator>
