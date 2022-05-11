673683720d7b0f7eee41e9b91d7649ca91e9c793
Parsh <parsh.cosmos11@gmail.com>
2022-05-11 14:50:43 +0530

    accounts saga: cleanup and realignment

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,7 +5,21 @@ import { composeWithDevTools } from '@redux-devtools/extension'
 import { persistStore, persistReducer } from 'redux-persist'
 import accountsReducer from './reducers/accounts'
 import storageReducer from './reducers/storage'
-import { addNewAccountShellsWatcher, importNewAccountWatcher, refreshAccountShellsWatcher, syncAccountsWatcher, loginWithHexaWatcher } from './sagas/accounts'
+import {
+  testcoinsWatcher,
+  syncAccountsWatcher,
+  generateSecondaryXprivWatcher,
+  resetTwoFAWatcher,
+  addNewAccountShellsWatcher,
+  importNewAccountWatcher,
+  refreshAccountShellsWatcher,
+  feeAndExchangeRatesWatcher,
+  autoSyncShellsWatcher,
+  validateTwoFAWatcher,
+  updateAccountSettingsWatcher,
+  restoreAccountShellsWatcher,
+} from './sagas/accounts'
+
 import { setupWalletWatcher } from './sagas/storage'
 import { reduxStorage  } from 'src/storage'
 
@@ -20,11 +34,19 @@ const rootSaga = function* () {
     setupWalletWatcher,
 
     // account watchers
-    addNewAccountShellsWatcher,
+    // accounts watchers
     syncAccountsWatcher,
+    testcoinsWatcher,
+    generateSecondaryXprivWatcher,
+    resetTwoFAWatcher,
+    feeAndExchangeRatesWatcher,
     refreshAccountShellsWatcher,
+    addNewAccountShellsWatcher,
     importNewAccountWatcher,
-    loginWithHexaWatcher
+    restoreAccountShellsWatcher,
+    autoSyncShellsWatcher,
+    validateTwoFAWatcher,
+    updateAccountSettingsWatcher,
   ]
 
   yield all(
