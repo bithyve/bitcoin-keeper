bc3c9520062a93cf3fa6a2f7c3b9689ec2f2244a
Pratyaksh <pratyaksh.chandra2898@gmail.com>
2022-06-06 17:02:37 +0530

    minor updates

--- src/store/sagas/uai.ts
+++ src/store/sagas/uai.ts
@@ -1,19 +1,8 @@
 import dbManager from 'src/storage/realm/dbManager';
 import { RealmSchema } from 'src/storage/realm/enum';
-import { realmConfig } from 'src/storage/realm/RealmProvider';
 import { ADD_TO_UAI_STACK, UPADTE_UAI_STACK } from '../actions/uai';
 import { createWatcher } from '../utilities';
 import { call } from 'redux-saga/effects';
-
-//Temp to test, will be removed
-export const updateRealm = async (schema: string, object: any) => {
-  const realm = await Realm.open(realmConfig);
-  if (realm) {
-    realm.write(() => {
-      realm.create(schema, object);
-    });
-  }
-};
 
 function* addToUaiStackWorker({ payload }) {
   const { uai } = payload;
@@ -21,14 +10,11 @@ function* addToUaiStackWorker({ payload }) {
     console.log('uai to db', uai);
     yield call(dbManager.initializeRealm, Buffer.from('random'));
     yield call(dbManager.createObject, RealmSchema.UAI, uai);
-    // dbManager.createObject(RealmSchema.UAI, uai);
   } catch (err) {
     console.error('Db add failed', err);
   }
-  // updateRealm(RealmSchema.UAI, uai);
 }
 
-//WIP
 function* updateUaiStackWorker({ payload }) {
   const { uai } = payload;
   console.log('uai to update', uai);
