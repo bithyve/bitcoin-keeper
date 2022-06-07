394ffb25a7e742c548bd8a9abbf2412cf710b983
Raheel1258 <rahilahmad1258@gmail.com>
2022-06-07 12:06:55 +0500

    apply types

--- src/screens/NewHomeScreen/WalletTab.tsx
+++ src/screens/NewHomeScreen/WalletTab.tsx
@@ -129,7 +129,7 @@ const WalletTab = ({ animate }) => {
     );
   };
 
-  const RenderWalletCard = ({ item }: { item: any }) => {
+  const RenderWalletCard = ({ item }: { item }) => {
     return (
       <WalletCard
         item={item}
