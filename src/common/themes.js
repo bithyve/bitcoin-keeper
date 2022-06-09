2787ae907e658a313bbcf2a8c311a594afea356b
Akshay Chavan <akshaychavan8551@gmail.com>
2022-06-09 13:25:27 +0530

    added color

--- src/common/themes.js
+++ src/common/themes.js
@@ -46,7 +46,8 @@ export const customTheme = extendTheme({
       KnowMoreButton: '#045647',
       dateText: '#50756E',
       Border: '#005545',
-      vaultCard: '#B2844E'
+      vaultCard: '#B2844E',
+      borderColor: '#EBB67A',
     },
     dark: {
       black: '#000000',
