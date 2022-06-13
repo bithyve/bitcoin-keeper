66f3b75b45342efea5eeb88a6d1a57b81bbbf5e0
Akshay Chavan <akshaychavan8551@gmail.com>
2022-06-13 14:23:28 +0530

    minor

--- src/screens/ChoosePlanScreen/ChoosePlan.tsx
+++ src/screens/ChoosePlanScreen/ChoosePlan.tsx
@@ -5,7 +5,6 @@ import { SafeAreaView, TouchableOpacity } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 
 import BackIcon from 'src/assets/icons/back.svg';
-import CurrencyTypeSwitch from 'src/components/Switch/CurrencyTypeSwitch';
 import ChoosePlanCarousel from 'src/components/Carousel/ChoosePlanCarousel';
 import Note from 'src/components/Note/Note';
 import DotView from 'src/components/DotView';
@@ -26,41 +25,43 @@ const ChoosePlan = (props) => {
       </Box>
       <Box ml={10} mb={5} flexDirection={'row'} w={'100%'} alignItems={'center'}>
         <Box w={'60%'}>
-          <Text fontSize={RFValue(20)} color={'light.textBlack'}>
+          <Text fontSize={RFValue(20)} color={'light.textBlack'} fontFamily={'heading'}>
             Choose your plan
           </Text>
-          <Text fontSize={RFValue(12)} color={'light.GreyText'}>
+          <Text fontSize={RFValue(12)} color={'light.GreyText'} fontFamily={'body'}>
             Lorem ipsum dolor sit amet{' '}
           </Text>
         </Box>
-        {/* <Box alignItems={'center'} justifyContent={'center'} w={'30%'}>
-          <CurrencyTypeSwitch />
-        </Box> */}
       </Box>
       <ChoosePlanCarousel />
       <Box mx={10} my={5}>
-        <Text fontSize={RFValue(14)} color={'light.lightBlack'} fontWeight={'bold'}>
+        <Text
+          fontSize={RFValue(14)}
+          color={'light.lightBlack'}
+          fontWeight={'bold'}
+          fontFamily={'body'}
+        >
           Benefits of going Pro
         </Text>
-        <Text fontSize={RFValue(12)} color={'light.GreyText'}>
+        <Text fontSize={RFValue(12)} color={'light.GreyText'} fontFamily={'body'}>
           Lorem ipsum dolor sit amet
         </Text>
       </Box>
       <Box mx={12}>
         <Box flexDirection={'row'} alignItems={'center'}>
-          <DotView height={2} width={2} />
-          <Text fontSize={RFValue(13)} color={'light.GreyText'} mb={2} ml={3}>
+          <DotView height={2} width={2} color={'black'} />
+          <Text fontSize={RFValue(13)} color={'light.GreyText'} mb={2} ml={3} fontFamily={'body'}>
             incididunt ut labore et dolore magna
           </Text>
         </Box>
         <Box flexDirection={'row'} alignItems={'center'}>
-          <DotView height={2} width={2} />
+          <DotView height={2} width={2} color={'black'} />
           <Text fontSize={RFValue(13)} color={'light.GreyText'} mb={2} ml={3}>
             aliqua. Ut enim ad minim veniam
           </Text>
         </Box>
         <Box flexDirection={'row'} alignItems={'center'}>
-          <DotView height={2} width={2} />
+          <DotView height={2} width={2} color={'black'} />
           <Text fontSize={RFValue(13)} color={'light.GreyText'} mb={2} ml={3}>
             quis nostrud exercitation ullamco
           </Text>
