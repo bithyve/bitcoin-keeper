4483371db10d2f1a82c4f372ee8c6bbd6bed8011
Bhumika Reddy <reddybhumi1999@gmail.com>
2022-06-22 15:53:58 +0530

    ui changes

--- src/screens/Inheritance/InheritanceSetup.tsx
+++ src/screens/Inheritance/InheritanceSetup.tsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { Box, Text, ScrollView, StatusBar, useColorMode, Pressable } from 'native-base';
-import { SafeAreaView, TouchableOpacity, View } from 'react-native';
-
+import React, { useContext } from 'react';
+import { Box, Text, ScrollView, StatusBar } from 'native-base';
+import { SafeAreaView, TouchableOpacity } from 'react-native';
+import InheritanceCard from './InheritanceCard';
 import BackIcon from 'src/assets/icons/back.svg';
 import { RFValue } from 'react-native-responsive-fontsize';
 import { LocalizationContext } from 'src/common/content/LocContext';
@@ -15,10 +15,10 @@ const InheritanceSetup = ({ navigation }) => {
     <SafeAreaView
       style={{
         flex: 1,
-        backgroundColor: '#F7F2EC',
+        backgroundColor: 'light.ReceiveBackground',
       }}
     >
-      <StatusBar backgroundColor={'#F7F2EC'} barStyle="dark-content" />
+      <StatusBar backgroundColor={'light.ReceiveBackground'} barStyle="dark-content" />
       <ScrollView>
       <Box mx={10} my={10}>
         <TouchableOpacity onPress={() => navigation.goBack()}>
@@ -30,126 +30,41 @@ const InheritanceSetup = ({ navigation }) => {
           {inheritence.SetupInheritance}
         </Text>
       </Box>
-      <Box m={5}>
-      <Box bg={'light.lightYellow'} flexDirection={'row'} p={15} borderRadius={10}>
-        <View style={{
-       alignItems:'center',
-       justifyContent:'center',
-       width:30,
-       height:30,
-       backgroundColor:'#E3E3E3',
-       borderRadius:30,
-       marginRight: 20,
-       marginTop: 15
-     }}>
-            <Text fontWeight={'extrabold'}> 1 </Text>
-        </View>
-          <View style = {{flexDirection: 'column'}}>
-          <Text color={'light.headerText'} fontSize={RFValue(13)} fontFamily={'heading'}>
-          {inheritence.SetupInheritance}
-          </Text>
-          <Text color={'light.GreyText'} fontSize={RFValue(12)} marginRight={10} fontFamily={'body'}>
-              {inheritence.UpgradetoElitetier}
-            </Text>
-          </View>
-        </Box>
-      </Box>
-      <Box m={5}>
-      <Box bg={'light.lightYellow'} flexDirection={'row'} p={15} borderRadius={10}>
-        <View style={{
-       alignItems:'center',
-       justifyContent:'center',
-       width:30,
-       height:30,
-       backgroundColor:'#E3E3E3',
-       borderRadius:30,
-       marginRight: 20,
-       marginTop: 35
-     }}>
-            <Text fontWeight={'extrabold'}> 2 </Text>
-        </View>
-          <View style = {{flexDirection: 'column'}}>
-          <Text color={'light.headerText'} fontSize={RFValue(13)} fontFamily={'heading'}>
-           {inheritence.ActivateInheritance}
-          </Text>
-          <Text color={'light.GreyText'} fontSize={RFValue(12)} marginRight={10} fontFamily={'body'}>
-             {inheritence.Downloadandsafelykeepinheritencedocuments}
-            </Text>
-          </View>
-        </Box>
-      </Box>
-      <Box m={5}>
-      <Box bg={'light.lightYellow'} flexDirection={'row'} p={15} borderRadius={10}>
-        <View style={{
-       alignItems:'center',
-       justifyContent:'center',
-       width:30,
-       height:30,
-       backgroundColor:'#E3E3E3',
-       borderRadius:30,
-       marginRight: 20,
-       marginTop: 25
-     }}>
-            <Text fontWeight={'extrabold'}> 3 </Text>
-        </View>
-          <View style = {{flexDirection: 'column'}}>
-          <Text color={'light.headerText'} fontSize={RFValue(13)} fontFamily={'heading'}>
-           {inheritence.Setupfalserecoveryalert}
-          </Text>
-          <Text color={'light.GreyText'} fontSize={RFValue(12)} marginRight={10} fontFamily={'body'}>
-              {inheritence.tryingtorecoveryourwallet}
-            </Text>
-          </View>
-        </Box>
-      </Box>
-      <Box m={5}>
-      <Box bg={'light.lightYellow'} flexDirection={'row'} p={15} borderRadius={10}>
-        <View style={{
-       alignItems:'center',
-       justifyContent:'center',
-       width:30,
-       height:30,
-       backgroundColor:'#E3E3E3',
-       borderRadius:30,
-       marginRight: 20,
-       marginTop: 25
-     }}>
-            <Text fontWeight={'extrabold'}> 4 </Text>
-        </View>
-          <View style = {{flexDirection: 'column'}}>
-          <Text color={'light.headerText'} fontSize={RFValue(13)} fontFamily={'heading'}>
-           {inheritence.IndependentRecovery}
-          </Text>
-          <Text color={'light.GreyText'} fontSize={RFValue(12)} marginRight={10} fontFamily={'body'}>
-              {inheritence.UnderstandhowyoucanrecoveryourVault}
-            </Text>
-          </View>
-        </Box>
-      </Box>
-      <Box m={5}>
-      <Box bg={'light.lightYellow'} flexDirection={'row'} p={15} borderRadius={10}>
-        <View style={{
-       alignItems:'center',
-       justifyContent:'center',
-       width:30,
-       height:30,
-       backgroundColor:'#E3E3E3',
-       borderRadius:30,
-       marginRight: 20,
-       marginTop: 15
-     }}>
-            <Text fontWeight={'extrabold'}> 5 </Text>
-        </View>
-          <View style = {{flexDirection: 'column'}}>
-          <Text color={'light.headerText'} fontSize={RFValue(13)} fontFamily={'heading'}>
-           {inheritence.Practicehealthcheck}
-          </Text>
-          <Text color={'light.GreyText'} fontSize={RFValue(12)} marginRight={10} fontFamily={'body'}>
-              {inheritence.Makessureyousignersareaccessible}
-            </Text>
-          </View>
-        </Box>
-      </Box>
+      <InheritanceCard
+            number={1}
+            title={inheritence.SetupInheritance}
+            description={inheritence.UpgradetoElitetier}
+            my={2}
+            icon={false}
+        />
+        <InheritanceCard
+            number={2}
+            title={inheritence.ActivateInheritance}
+            description={inheritence.Downloadandsafelykeepinheritencedocuments}
+            my={2}
+            icon={false}
+        />
+        <InheritanceCard
+            number={3}
+            title={inheritence.Setupfalserecoveryalert}
+            description={inheritence.tryingtorecoveryourwallet}
+            my={2}
+            icon={false}
+        />
+        <InheritanceCard
+            number={4}
+            title={inheritence.IndependentRecovery}
+            description={inheritence.UnderstandhowyoucanrecoveryourVault}
+            my={2}
+            icon={false}
+        />
+        <InheritanceCard
+            number={5}
+            title={inheritence.Practicehealthcheck}
+            description={inheritence.Makessureyousignersareaccessible}
+            my={2}
+            icon={false}
+        />
       </ScrollView>
     </SafeAreaView>
   );
