535f5281a3286ffa2cc72c26d851eb97793e5fff
Raheel1258 <rahilahmad1258@gmail.com>
2022-06-28 14:08:54 +0500

    Set up vault:Text change. #465

--- src/screens/NewHomeScreen/HomeScreen.tsx
+++ src/screens/NewHomeScreen/HomeScreen.tsx
@@ -149,9 +149,9 @@ const VaultSetupContent = () => {
           'For the Basic tier, you need to select one Signer to activate your Vault. This can be upgraded to 3 Signers and 5 Signers when on Expert or Elite tier respectively'
         }
       </Text>
-      <Text color={'white'} fontSize={13} fontFamily={'body'} fontWeight={'200'} p={2}>
+      {/* <Text color={'white'} fontSize={13} fontFamily={'body'} fontWeight={'200'} p={2}>
         {'To get started, you need to add a Signer (hardware wallet or a signer device) to Keeper'}
-      </Text>
+      </Text> */}
     </View>
   );
 };
