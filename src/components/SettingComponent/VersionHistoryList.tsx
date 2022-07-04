6680f1879024df799fa260adb272ddd3b0e8d28e
Akshay Chavan <akshaychavan8551@gmail.com>
2022-07-04 14:24:28 +0530

    resolved - #436

--- src/components/SettingComponent/VersionHistoryList.tsx
+++ src/components/SettingComponent/VersionHistoryList.tsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { FlatList, Box, Text, ScrollView, StatusBar, useColorMode, Pressable } from 'native-base';
+import React from 'react';
+import { FlatList, Box, Text } from 'native-base';
 
 import { RFValue } from 'react-native-responsive-fontsize';
+import DotView from '../DotView';
 const data = [
   {
     id: 1,
@@ -9,40 +10,72 @@ const data = [
     comment1: 'App Enhancements',
     comment2: 'Bug Fixes',
     comment3: 'New Integration - Swan Bitcoin',
+    date: '20 June ’21',
   },
   {
     id: 2,
     versionTitle: 'Version 1.0.8',
     comment1: 'App Enhancements',
     comment2: 'Bug Fixes',
+    date: '15 March ’21',
   },
   {
     id: 3,
     versionTitle: 'Version 1.0.7',
     comment1: 'App Enhancements',
     comment2: 'Bug Fixes',
+    date: '08 January ’21',
+  },
+  {
+    id: 4,
+    versionTitle: 'Version 1.0.6',
+    comment1: 'App Enhancements',
+    comment2: 'Bug Fixes',
+    date: '28 December ’20',
   },
 ];
 const VersionHistoryList = () => {
   return (
     <FlatList
+      style={{ overflow: 'visible' }}
       data={data}
       renderItem={({ item }) => (
-        <Box bg={'light.lightYellow'} p={5} borderRadius={10} my={2}>
-          <Text color={'light.headerText'} fontSize={RFValue(14)} fontFamily={'heading'}>
-            {item.versionTitle}
+        <Box borderLeftColor={'#E3BE96'} borderLeftWidth={1} w={'100%'} position="relative">
+          <Box
+            zIndex={99}
+            position={'absolute'}
+            left={-8}
+            bg={'light.ReceiveBackground'}
+            p={1}
+            borderRadius={15}
+          >
+            <DotView height={2} width={2} color={'#E3BE96'} />
+          </Box>
+          <Text
+            color={'light.GreyText'}
+            fontSize={RFValue(10)}
+            fontWeight={'300'}
+            ml={5}
+            opacity={0.7}
+          >
+            {item.date}
           </Text>
-          <Text color={'light.GreyText'} fontSize={RFValue(12)} fontFamily={'body'}>
-            {item.comment1}
-          </Text>
-          <Text color={'light.GreyText'} fontSize={RFValue(12)} fontFamily={'body'}>
-            {item.comment2}
-          </Text>
-          {item.comment3 ? (
-            <Text color={'light.GreyText'} fontSize={RFValue(12)} fontFamily={'body'}>
-              {item.comment3}
+          <Box bg={'light.lightYellow'} p={5} borderRadius={10} my={2} ml={5}>
+            <Text color={'light.headerText'} fontSize={RFValue(14)} fontFamily={'heading'}>
+              {item.versionTitle}
             </Text>
-          ) : null}
+            <Text color={'light.GreyText'} fontSize={RFValue(12)} fontFamily={'body'}>
+              {item.comment1}
+            </Text>
+            <Text color={'light.GreyText'} fontSize={RFValue(12)} fontFamily={'body'}>
+              {item.comment2}
+            </Text>
+            {item.comment3 ? (
+              <Text color={'light.GreyText'} fontSize={RFValue(12)} fontFamily={'body'}>
+                {item.comment3}
+              </Text>
+            ) : null}
+          </Box>
         </Box>
       )}
       keyExtractor={(item) => item.id}
