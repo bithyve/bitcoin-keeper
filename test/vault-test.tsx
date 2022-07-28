ae7c1b593e173cc1622c6a34b2729c299786941e
gourab <biz.gourab@gmail.com>
2022-07-28 11:54:41 +0530

    send confirmation test case

--- test/vault-test.tsx
+++ test/vault-test.tsx
@@ -1,9 +1,9 @@
 import 'react-native';
 import React from 'react';
-import render from 'react-test-renderer';
+import renderer from 'react-test-renderer';
 import SendConfirmation from 'src/screens/Send/SendConfirmation';
 
 test('Send Confirmation', () => {
-  const snap = render.create(<SendConfirmation route />).toJSON();
+  const snap = renderer.create(<SendConfirmation route />).toJSON();
   expect(snap).toMatchInlineSnapshot();
 });
