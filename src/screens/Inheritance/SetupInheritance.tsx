2d5582656f493b5807a11b553dec25edca4deaee
Raheel1258 <rahilahmad1258@gmail.com>
2022-07-28 19:12:11 +0500

    add inheritance modal, #509

--- src/screens/Inheritance/SetupInheritance.tsx
+++ src/screens/Inheritance/SetupInheritance.tsx
@@ -1,36 +1,92 @@
-import React from 'react';
-import { Box, Text, Pressable, FlatList } from 'native-base';
-import { RFValue } from 'react-native-responsive-fontsize';
+import React, { useState } from 'react';
+import { Box, Text } from 'native-base';
 import { ScaledSheet } from 'react-native-size-matters';
 import { useNavigation } from '@react-navigation/native';
-//components and functions
-import Header from 'src/components/Header';
-import StatusBarComponent from 'src/components/StatusBarComponent';
-import { wp, hp } from 'src/common/data/responsiveness/responsive';
-
-import Abc from 'src/assets/images/illustration.svg';
-import Inheritance from 'src/assets/images/svgs/inheritance_Inner.svg';
 import LinearGradient from 'react-native-linear-gradient';
+//components and functions
+import { wp, hp } from 'src/common/data/responsiveness/responsive';
+import StatusBarComponent from 'src/components/StatusBarComponent';
+import Header from 'src/components/Header';
 import Buttons from 'src/components/Buttons';
 import Note from 'src/components/Note/Note';
+import KeeperModal from 'src/components/KeeperModal';
+// icons and asserts
+import Assert from 'src/assets/images/illustration.svg';
+import Vault from 'src/assets/images/svgs/vault.svg';
+import SettingUp from 'src/assets/images/svgs/settingup.svg';
+import Recovery from 'src/assets/images/svgs/recovery.svg';
+import Inheritance from 'src/assets/images/svgs/inheritance_Inner.svg';
 
 const SetupInheritance = () => {
   const navigtaion = useNavigation();
-
+  const [modalVisiblity, setModalVisiblity] = useState(true);
   const inheritanceData = [
     {
       title: 'Setup Vault',
-      subTitle: 'Upgrade the Vault to Elite with 5 Signers'
+      subTitle: 'Upgrade the Vault to Elite with 5 Signing Devices Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
+      Icon: Vault
     },
     {
       title: 'Activate Inheritance',
-      subTitle: 'Download documents: Will Addendum for Digital Assets and Recovery Instructions. Understand safekeeping best practices'
+      subTitle: 'Download documents: Will Addendum for Digital Assets and Recovery Instructions. Understand safekeeping best practices',
+      Icon: SettingUp
     },
     {
       title: 'Independent Recovery (optional)',
-      subTitle: 'Understand how you can recover your Vault even without teh Keeper app or any service from the company'
+      subTitle: 'Understand how you can recover your Vault even without teh Keeper app or any service from the company',
+      Icon: Recovery
+
     }
   ]
+
+  const InheritancePoint = ({ title, description, Icon }) => {
+    return (
+      <Box marginBottom={hp(20)}>
+        <Box
+          width={hp(300)}
+          flexDir={'row'}
+          alignItems={'center'}
+        >
+          <Icon />
+          <Text
+            color={'light.white1'}
+            fontSize={15}
+            marginLeft={wp(15)}
+            numberOfLines={2}
+            alignItems={'center'}
+          >
+            {title}
+          </Text>
+        </Box>
+        <Text
+          color={'light.white1'}
+          fontSize={15}
+          marginTop={wp(16)}
+          alignItems={'center'}
+          width={wp(300)}
+        >
+          {description}
+        </Text>
+      </Box>
+    )
+  }
+
+  const InheritanceContent = () => {
+    return (
+      <Box >
+        {inheritanceData.map((item) => {
+          return (
+            <InheritancePoint
+              title={item.title}
+              description={item.subTitle}
+              Icon={item.Icon}
+            />
+          );
+        })}
+      </Box>
+    )
+  }
+
   const GradientIcon = ({ height, Icon }) => {
     return (
       <LinearGradient
@@ -91,7 +147,7 @@ const SetupInheritance = () => {
         marginTop={hp(50)}
         alignItems={'center'}
       >
-        <Abc />
+        <Assert />
         <Text
           color={'light.lightBlack2'}
           fontSize={13}
@@ -117,6 +173,18 @@ const SetupInheritance = () => {
           subtitleColor={'GreyText'}
         />
       </Box>
+      <KeeperModal
+        visible={modalVisiblity}
+        close={() => { setModalVisiblity(false) }}
+        title={'Inheritance'}
+        subTitle={'Securely bequeath your bitcoin'}
+        modalBackground={['#00836A', '#073E39']}
+        textColor={'#FFFFFF'}
+        buttonText={'Add Now'}
+        buttonTextColor={'#073E39'}
+        buttonBackground={['#FFFFFF', '#80A8A1']}
+        Content={InheritanceContent}
+      />
     </Box >
   );
 };
