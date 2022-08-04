22c5741c0b661a3f3a81534dffe7a0b8d781662d
PraneethGunas <praneethgunas404@gmail.com>
2022-08-05 03:04:14 +0530

    Added ledger client

---
+++ src/hardware/ledger/index.ts
@@ -0,0 +1,8 @@
+import { DefaultWalletPolicy, WalletPolicy } from './client/policy';
+
+import AppClient from './client/appClient';
+import { PsbtV2 } from './client/psbtv2';
+
+export { AppClient, PsbtV2, DefaultWalletPolicy, WalletPolicy };
+
+export default AppClient;
