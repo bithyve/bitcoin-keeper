2d25c14d8e485a05044bce84c7ff1b656f1f6309
PraneethGunas <praneethgunas404@gmail.com>
2022-08-05 16:40:59 +0530

    Common BLE manager

---
+++ src/core/services/ble/index.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+import { BleManager } from 'react-native-ble-plx';
+
+export const manager = new BleManager();
+
+export const useBleDevices = (deviceName: string = '') => {
+  const [devices, setDevices] = useState([]);
+
+  const scanAndConnect = () => {
+    manager.startDeviceScan(null, null, (error, device) => {
+      if (error) {
+        console.log(error);
+        // scanning will be stopped automatically
+        return;
+      }
+      setDevices([...devices, device]);
+
+      // stopDeviceScan criteria.
+      if (device.name === deviceName) {
+        // Stop scanning as it's not necessary if you are scanning for one device.
+        manager.stopDeviceScan();
+      }
+    });
+  };
+
+  React.useEffect(() => {
+    const subscription = manager.onStateChange((state) => {
+      if (state === 'PoweredOn') {
+        scanAndConnect();
+        subscription.remove();
+      }
+    }, true);
+    return () => subscription.remove();
+  }, [manager]);
+
+  return { devices };
+};
