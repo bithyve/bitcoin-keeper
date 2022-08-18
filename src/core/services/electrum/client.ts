4acacede5b0fb6ddc0167f8a6cc9ba0d5d410b08
Parsh <parsh.cosmos11@gmail.com>
2022-08-18 13:59:13 +0530

    electrum client: batch test

--- src/core/services/electrum/client.ts
+++ src/core/services/electrum/client.ts
@@ -2,10 +2,11 @@ import ElectrumClient from 'electrum-client';
 import net from 'react-native-tcp';
 
 export const testElectrumClient = async () => {
-  const client = new ElectrumClient(net, false, 60001, 'btc.electroncash.dk', 'tcp');
+  const client = new ElectrumClient(net, false, 50001, 'api.bithyve.com', 'tcp');
   const ver = await client.initElectrum({ client: 'bluewallet', version: '1.4' });
-  const balance = await client.blockchainScripthash_getBalance(
-    '716decbe1660861c3d93906cb1d98ee68b154fd4d23aed9783859c1271b52a9c'
-  );
-  console.log({ ver, balance });
+  const balance = await client.blockchainScripthash_getBalanceBatch([
+    '716decbe1660861c3d93906cb1d98ee68b154fd4d23aed9783859c1271b52a9c',
+    '716decbe1660861c3d93906cb1d98ee68b154fd4d23aed9783859c1271b52a9c',
+  ]);
+  balance.forEach(console.log);
 };
