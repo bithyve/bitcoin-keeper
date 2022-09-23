864c4e77ca217224fd9e03769610c193c174f545
Raheel Ahmad <rahilahmad1258@gmail.com>
2022-09-23 12:20:05 +0500

    Setup your vault screen: UI issue. #897 (#955)
    * Setup your vault screen: UI issue. #897
    
    * SD list #906
    
    * add messages
    
    * add rules to add signing keys
    
    * update some code
    
    * add hook for signinglist
    
    * resolve small bug
    
    * fix some bugs

--- src/screens/Vault/WalletMap.tsx
+++ src/screens/Vault/WalletMap.tsx
@@ -1,4 +1,9 @@
-import APP from 'src/assets/images/app.svg';
+import React from 'react';
+import { Text } from 'native-base';
+
+import { SignerType, SignerStorage } from 'src/core/wallets/enums';
+import { getNfcSupport, getBluetoothSupport, getDisabled } from './SigningDeviceList';
+
 import COLDCARDICON from 'src/assets/images/coldcard_icon.svg';
 import COLDCARDICONLIGHT from 'src/assets/icons/coldcard_light.svg';
 import COLDCARDLOGO from 'src/assets/images/coldcard_logo.svg';
@@ -11,28 +16,33 @@ import LEDGERICONLIGHT from 'src/assets/icons/ledger_light.svg';
 import LEDGERLOGO from 'src/assets/images/ledger_logo.svg';
 import PASSPORTICON from 'src/assets/images/passport_icon.svg';
 import PASSPORTLOGO from 'src/assets/images/passport_logo.svg';
-import React from 'react';
-import SERVER from 'src/assets/images/server.svg';
-import { SignerType } from 'src/core/wallets/enums';
 import TAPSIGNERICON from 'src/assets/images/tapsigner_icon.svg';
 import TAPSIGNERICONLIGHT from 'src/assets/icons/tapsigner_light.svg';
 import TAPSIGNERLOGO from 'src/assets/images/tapsigner_logo.svg';
 import TREZORICON from 'src/assets/images/trezor_icon.svg';
 import TREZORICONLIGHT from 'src/assets/icons/trezor_light.svg';
 import TREZORLOGO from 'src/assets/images/trezor_logo.svg';
-import { Text } from 'native-base';
+import APP from 'src/assets/images/app.svg';
+import SERVER from 'src/assets/images/server.svg';
 
 export const WalletMap = (type: SignerType, light = false) => {
+
   switch (type) {
     case SignerType.COLDCARD:
       return {
         Icon: light ? <COLDCARDICONLIGHT /> : <COLDCARDICON />,
         Logo: <COLDCARDLOGO />,
+        disable: getNfcSupport(),
+        message: getNfcSupport() ? 'NFC is Not enabled in your device' : '',
+        type: SignerStorage.COLD
       };
     case SignerType.JADE:
       return {
         Icon: <JADEICON />,
         Logo: <JADELOGO />,
+        disable: false,
+        message: '',
+        type: SignerStorage.COLD
       };
     case SignerType.KEEPER:
       return {
@@ -42,49 +52,76 @@ export const WalletMap = (type: SignerType, light = false) => {
             Another Keeper App
           </Text>
         ),
+        disable: false,
+        message: ''
       };
     case SignerType.KEYSTONE:
       return {
         Icon: <KEYSTONEICON />,
         Logo: <KEYSTONELOGO />,
+        disable: false,
+        message: '',
+        type: SignerStorage.COLD
       };
     case SignerType.LEDGER:
       return {
         Icon: light ? <LEDGERICONLIGHT /> : <LEDGERICON />,
         Logo: <LEDGERLOGO />,
+        disable: !getBluetoothSupport(),
+        message: !getBluetoothSupport() ? 'BLE is Not enabled in your device' : '',
+        type: SignerStorage.COLD
       };
     case SignerType.MOBILE_KEY:
       return {
         Icon: <APP />,
-        Logo: (
-          <Text letterSpacing={1.5} fontWeight={200} fontSize={14} color={'light.lightBlack2'}>
-            Mobile Key
-          </Text>
-        ),
+        Logo: <Text
+          letterSpacing={1.5}
+          fontWeight={200}
+          fontSize={14}
+          color={'light.lightBlack2'}>
+          Mobile Key
+        </Text>,
+        disable: getDisabled().disabled,
+        message: getDisabled().message,
+        type: SignerStorage.HOT
       };
     case SignerType.PASSPORT:
       return {
         Icon: <PASSPORTICON />,
         Logo: <PASSPORTLOGO />,
+        disable: false,
+        message: '',
+        type: SignerStorage.COLD
       };
     case SignerType.POLICY_SERVER:
       return {
         Icon: <SERVER />,
-        Logo: (
-          <Text letterSpacing={1.5} fontWeight={200} fontSize={14} color={'light.lightBlack2'}>
-            Signing Server
-          </Text>
-        ),
+        Logo: <Text
+          letterSpacing={1.5}
+          fontWeight={200}
+          fontSize={14}
+          color={'light.lightBlack2'}>
+          Signing Server
+        </Text>,
+        disable: getDisabled().disabled,
+        message: getDisabled().message,
+        type: SignerStorage.HOT
       };
     case SignerType.TAPSIGNER:
       return {
         Icon: light ? <TAPSIGNERICONLIGHT /> : <TAPSIGNERICON />,
         Logo: <TAPSIGNERLOGO />,
+        disable: getNfcSupport(),
+        message: getNfcSupport() ? 'NFC is Not enabled in your device' : '',
+        type: SignerStorage.COLD
       };
     case SignerType.TREZOR:
       return {
         Icon: light ? <TREZORICONLIGHT /> : <TREZORICON />,
         Logo: <TREZORLOGO />,
+        disable: false,
+        message: '',
+        type: SignerStorage.COLD
       };
     case SignerType.SEED_WORDS:
       return {
@@ -94,11 +131,17 @@ export const WalletMap = (type: SignerType, light = false) => {
             Seed Words Based
           </Text>
         ),
+        disable: getDisabled().disabled,
+        message: getDisabled().message,
+        type: SignerStorage.WARM
       };
     default:
       return {
         Icon: null,
         Logo: null,
+        disable: false,
+        message: ''
       };
   }
 };
+
