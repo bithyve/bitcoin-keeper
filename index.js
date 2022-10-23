303328e6618b67cb262cc302a8d2db783bcaa945
Raheel1258 <rahilahmad1258@gmail.com>
2022-10-23 15:51:33 +0500

    adjust font scalling

--- index.js
+++ index.js
@@ -4,12 +4,18 @@
 import 'react-native-reanimated';
 import 'react-native-gesture-handler';
 import './shim';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, Text as NativeText } from 'react-native';
 import 'react-native-get-random-values';
 import App from './App';
 import { name as appName } from './app.json';
 import { enableAndroidFontFix } from './AndroidFontFix';
+import { Text } from "native-base";
 
 enableAndroidFontFix();
+
+Text.defaultProps = Text.defaultProps || {};
+Text.defaultProps.allowFontScaling = false;
+NativeText.defaultProps = Text.defaultProps || {};
+NativeText.defaultProps.allowFontScaling = false;
 
 AppRegistry.registerComponent(appName, () => App);
