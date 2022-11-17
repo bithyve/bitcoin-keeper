e7eda4db79b0d6f2206c03efd36aeaed444d69ec
Akshay Chavan <akshaychavan8551@gmail.com>
2022-11-17 15:02:58 +0530

    designed modal UI - #1214

--- src/screens/Send/SendConfirmation.tsx
+++ src/screens/Send/SendConfirmation.tsx
@@ -22,6 +22,7 @@ import { RealmSchema } from 'src/storage/realm/enum';
 import { RealmWrapperContext } from 'src/storage/realm/RealmProvider';
 import ScreenWrapper from 'src/components/ScreenWrapper';
 import SuccessIcon from 'src/assets/images/svgs/successSvg.svg';
+import VaultIcon from 'src/assets/icons/vault_setup.svg';
 import { TxPriority } from 'src/core/wallets/enums';
 import { Vault } from 'src/core/wallets/interfaces/vault';
 import { Wallet } from 'src/core/wallets/interfaces/wallet';
@@ -37,6 +38,7 @@ import { useDispatch } from 'react-redux';
 import useFormattedAmountText from 'src/hooks/formatting/UseFormattedAmountText';
 import useFormattedUnitText from 'src/hooks/formatting/UseFormattedUnitText';
 import { windowHeight } from 'src/common/data/responsiveness/responsive';
+import KeeperModal from 'src/components/KeeperModal';
 
 const SendConfirmation = ({ route }) => {
   const navigtaion = useNavigation();
@@ -61,6 +63,7 @@ const SendConfirmation = ({ route }) => {
     walletToWallet = false,
   } = uiMetaData;
   const txFeeInfo = useAppSelector((state) => state.sendAndReceive.transactionFeeInfo);
+  const successSatus = useAppSelector((state) => state.sendAndReceive.sendPhaseTwo.isSuccessful);
   const [transactionPriority, setTransactionPriority] = useState(TxPriority.LOW);
   const { useQuery } = useContext(RealmWrapperContext);
   const wallets: Wallet[] = useQuery(RealmSchema.Wallet).map(getJSONFromRealmObject);
@@ -75,6 +78,8 @@ const SendConfirmation = ({ route }) => {
   const { translations } = useContext(LocalizationContext);
   const common = translations['common'];
   const walletTransactions = translations['wallet'];
+
+  const [visibleModal, setVisibleModal] = useState(false);
 
   // // Sending process is still not executed
   // const [sendingModal, setSendingModal] = useState(false);
@@ -103,12 +108,36 @@ const SendConfirmation = ({ route }) => {
         <Box alignSelf={'center'}>
           <SuccessIcon />
         </Box>
-        <Text color={'#5F6965'} fontSize={13} fontFamily={'body'} fontWeight={'200'} p={2}>
-          {'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor'}
-        </Text>
-        <Text color={'5F6965'} fontSize={13} fontFamily={'body'} fontWeight={'200'} p={2}>
+        <Text color={'#073B36'} fontSize={13} fontFamily={'body'} fontWeight={'200'} p={2}>
           {
-            'To get started, you need to add a Signer (hardware wallet or a signer device) to Keeper'
+            'You can view the confirmation status of the transaction on any block explorer or when the vault transaction list is refreshed'
+          }
+        </Text>
+      </View>
+    );
+  };
+  //
+  const TransVaultSuccessfulContent = () => {
+    return (
+      <View>
+        <Box alignSelf={'center'}>
+          <VaultIcon />
+        </Box>
+        <Text color={'#073B36'} fontSize={13} fontFamily={'body'} fontWeight={'200'} p={2}>
+          {'The transaction should be visible in the vault in some time.'}
+        </Text>
+      </View>
+    );
+  };
+  const NewVaultActivateContent = () => {
+    return (
+      <View>
+        <Box alignSelf={'center'}>
+          <VaultIcon />
+        </Box>
+        <Text color={'#073B36'} fontSize={13} fontFamily={'body'} fontWeight={'200'} p={2}>
+          {
+            'Whenever you upgrade, downgrade or change signing devices, a new vault is created with the new set of keys'
           }
         </Text>
       </View>
@@ -123,6 +152,8 @@ const SendConfirmation = ({ route }) => {
   // };
 
   const onProceed = () => {
+    // console.log('pressed');
+    // setVisibleModal(true);
     // closeAllModal();
     if (isVaultTransfer) {
       if (sourceWallet.specs.balances.confirmed < sourceWallet.specs.transferPolicy) {
@@ -585,6 +616,43 @@ const SendConfirmation = ({ route }) => {
           primaryCallback={onProceed}
         />
       </Box>
+      {/*Modals */}
+      <KeeperModal
+        visible={visibleModal}
+        close={() => setVisibleModal(false)}
+        title={walletTransactions.SendSuccess}
+        subTitle={'The transaction has been successfully broadcasted'}
+        buttonText={walletTransactions.ViewDetails}
+        textColor={'#073B36'}
+        buttonTextColor={'#FAFAFA'}
+        // cancelButtonText={common.cancel}
+        // cancelButtonColor={'#073E39'}
+        Content={SendSuccessfulContent}
+        // buttonPressed={viewDetails}
+      />
+      {/* <KeeperModal
+        visible={visibleModal}
+        close={() => setVisibleModal(false)}
+        title={'Transfer to Vault Successfull'}
+        subTitle={'You have successfully transferred from your wallet to the vault'}
+        buttonText={'View Vault'}
+        textColor={'#073B36'}
+        buttonTextColor={'#FAFAFA'}
+        Content={TransVaultSuccessfulContent}
+      /> */}
+      {/* <KeeperModal
+        visible={visibleModal}
+        close={() => setVisibleModal(false)}
+        title={'New Vault activated!'}
+        subTitle={
+          'The new set of signing devices will be needed to sign transactions from this vault'
+        }
+        buttonText={'View Vault'}
+        textColor={'#073B36'}
+        buttonTextColor={'#FAFAFA'}
+        Content={NewVaultActivateContent}
+      /> */}
+      {/* end */}
 
       {/* {showOverlay && (
         <View
@@ -596,20 +664,7 @@ const SendConfirmation = ({ route }) => {
           position={'absolute'}
         ></View>
       )} */}
-      {/* <KeeperModal
-        visible={visible}
-        close={close}
-        title={walletTransactions.SendSuccess}
-        subTitle={'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}
-        buttonText={walletTransactions.ViewDetails}
-        textColor={'#073B36'}
-        buttonTextColor={'#FAFAFA'}
-        cancelButtonText={common.cancel}
-        cancelButtonColor={'#073E39'}
-        Content={SendSuccessfulContent}
-        buttonPressed={viewDetails}
-      />
-
+      {/* 
       <KeeperModal
         visible={sendingModal}
         close={closeSendModal}
