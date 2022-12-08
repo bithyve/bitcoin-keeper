8499f01863448adbe4def0425c51308459c2df6f
Parsh <parsh.cosmos11@gmail.com>
2022-12-08 15:26:12 +0530

    electrum-client: refactor

--- src/core/services/electrum/client.ts
+++ src/core/services/electrum/client.ts
@@ -1,96 +1,107 @@
-const ElectrumCli = require('electrum-client');
+/* eslint-disable no-console */
 import config from 'src/core/config';
 import { NetworkType } from 'src/core/wallets/enums';
-const reverse = require('buffer-reverse');
+import ElectrumCli from 'electrum-client';
 
+// const reverse = require('buffer-reverse');
 
-const defaultPeer = { host: '', ssl: '' };
-const predefinedPeers = [
+const ELECTRUM_CLIENT_CONFIG = {
+  predefinedTestnetPeers: [{ host: '35.177.46.45', ssl: '50002' }],
+  predefinedPeers: [
     // peers for production server goes here.
-];
+  ],
+  maxConnectionAttempt: 5,
+  reconnectDelay: 1000, // 1 second
+};
+export default class ElectrumClient {
+  public static electrumClient;
 
-const predefinedTestnetPeers = [
-    { host: '35.177.46.45', ssl: '50002' }
-];
+  public static isClientConnected = false;
 
-let electrumClient;
-let isClientConnected = false;
-let currentPeerIndex = Math.floor(Math.random() * predefinedPeers.length);
-let connectionAttempt = 0;
+  public static currentPeerIndex = Math.floor(
+    Math.random() * ELECTRUM_CLIENT_CONFIG.predefinedPeers.length
+  );
 
-export async function connect() {
-  const peer = await getNextPeer();
-  try {
-      console.log(peer);
-      console.log('Create electrum instance')
+  public static connectionAttempt = 0;
 
-      electrumClient = new ElectrumCli(global.net, global.tls, peer.ssl || peer.tcp, peer.host, peer.ssl ? 'tls' : 'tcp') ;// tcp or tls
-      console.log('Instance created')
+  public static async connect() {
+    const peer = await ElectrumClient.getNextPeer();
+    try {
+      ElectrumClient.electrumClient = new ElectrumCli(
+        global.net,
+        global.tls,
+        peer.ssl || peer.tcp,
+        peer.host,
+        peer.ssl ? 'tls' : 'tcp'
+      ); // tcp or tls
 
-      electrumClient.onError = (e) => {
-          console.log('Electrum mainClient.onError():', e.message);
+      ElectrumClient.electrumClient.onError = (error) => {
+        console.log('Electrum mainClient.onError():', error.message);
 
-          if (isClientConnected) {
-              electrumClient.close && electrumClient.close();
-              isClientConnected = false;
-              console.log("Error: Close the connection")
-              setTimeout(connect, 500);
-          }
-      }
-      console.log('Initiate electrum server')
-      const ver = await electrumClient.initElectrum({ client: 'nguwallet', version: '1.4' });
-      console.log('Connection to electrum server is established')
+        if (ElectrumClient.isClientConnected) {
+          if (ElectrumClient.electrumClient.close) ElectrumClient.electrumClient.close();
+
+          ElectrumClient.isClientConnected = false;
+          console.log('Error: Close the connection');
+          setTimeout(ElectrumClient.connect, ELECTRUM_CLIENT_CONFIG.reconnectDelay);
+        }
+      };
+      console.log('Initiate electrum server');
+      const ver = await ElectrumClient.electrumClient.initElectrum({
+        client: 'bitcoin-keeper',
+        version: '1.4',
+      });
+      console.log('Connection to electrum server is established', { ver });
       if (ver && ver[0]) {
-          console.log(`ver : ${ver}`);
-          isClientConnected = true;
+        console.log(`ver : ${ver}`);
+        ElectrumClient.isClientConnected = true;
       }
-  }
-  catch (e) {
-      isClientConnected = false;
-      console.log('Bad connection:', JSON.stringify(peer), e)
+    } catch (error) {
+      ElectrumClient.isClientConnected = false;
+      console.log('Bad connection:', JSON.stringify(peer), error);
+    }
+
+    if (ElectrumClient.isClientConnected) return ElectrumClient.isClientConnected;
+    return ElectrumClient.reconnect();
   }
 
-  if (!isClientConnected) {
-      console.log('Attempt to retry');
-      connectionAttempt = connectionAttempt + 1;
-      console.log("Close the connection before attempting again");
-      electrumClient.close && electrumClient.close();
-      if (connectionAttempt >= 5) {
-          console.log('Could not find the working electrum server. Please try again later.');
-      }
-      else {
-          console.log(`Reconnection attempt #${connectionAttempt}`)
-          await new Promise(resolve => setTimeout(resolve, 500));
-          return connect();
-      }
-  }
-}
+  public static async reconnect() {
+    console.log('Trying to reconnect');
+    ElectrumClient.connectionAttempt += 1;
 
-async function getCurrentPeer() {
-  const isTestnet = config.NETWORK_TYPE == NetworkType.TESTNET;
-  return isTestnet ? predefinedTestnetPeers[currentPeerIndex] : predefinedPeers[currentPeerIndex];
-}
+    // close the connection before attempting again
+    if (ElectrumClient.electrumClient.close) ElectrumClient.electrumClient.close();
 
-async function getNextPeer() {
-  const isTestnet = config.NETWORK_TYPE == NetworkType.TESTNET;
-  currentPeerIndex++;
-  if (currentPeerIndex + 1 > (isTestnet ? predefinedTestnetPeers.length : predefinedPeers.length)) {
-      currentPeerIndex = 0;
+    if (ElectrumClient.connectionAttempt >= ELECTRUM_CLIENT_CONFIG.maxConnectionAttempt) {
+      console.log('Could not find the working electrum server. Please try again later.');
+      return ElectrumClient.isClientConnected; // false
+    }
+    console.log(`Reconnection attempt #${ElectrumClient.connectionAttempt}`);
+    await new Promise((resolve) => {
+      setTimeout(resolve, ELECTRUM_CLIENT_CONFIG.reconnectDelay); // attempts reconnection after 1 second
+    });
+    return ElectrumClient.connect();
   }
-  const peer = getCurrentPeer();
-  return peer;
-}
 
-export async function ping() {
-  if (electrumClient) {
-      try {
-          await electrumClient.server_ping();
-      }
-      catch (ex) {
-          isClientConnected = false;
-          return false;
-      }
-      return true;
+  public static getCurrentPeer() {
+    const isTestnet = config.NETWORK_TYPE === NetworkType.TESTNET;
+    return isTestnet
+      ? ELECTRUM_CLIENT_CONFIG.predefinedTestnetPeers[ElectrumClient.currentPeerIndex]
+      : ELECTRUM_CLIENT_CONFIG.predefinedPeers[ElectrumClient.currentPeerIndex];
   }
-  return false;
+
+  public static getNextPeer() {
+    const isTestnet = config.NETWORK_TYPE === NetworkType.TESTNET;
+    ElectrumClient.currentPeerIndex += 1;
+    if (
+      ElectrumClient.currentPeerIndex >
+      (isTestnet
+        ? ELECTRUM_CLIENT_CONFIG.predefinedTestnetPeers.length - 1
+        : ELECTRUM_CLIENT_CONFIG.predefinedPeers.length - 1)
+    )
+      ElectrumClient.currentPeerIndex = 0; // reset(out of bounds)
+
+    const peer = ElectrumClient.getCurrentPeer();
+    return peer;
+  }
 }
