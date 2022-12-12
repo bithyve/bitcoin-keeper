e5a7f69627f94c1fca6283c77893adf12b731814
Pratyaksh Chandra <pratyaksh.chandra2898@gmail.com>
2022-12-13 04:13:43 +0530

    Add Amount screen cleanup

--- src/screens/Send/AddSendAmount.tsx
+++ src/screens/Send/AddSendAmount.tsx
@@ -7,7 +7,8 @@ import { hp, windowHeight, windowWidth, wp } from 'src/common/data/responsivenes
 import AppNumPad from 'src/components/AppNumPad';
 import Buttons from 'src/components/Buttons';
 import Colors from 'src/theme/Colors';
-import DollarInput from 'src/assets/images/svgs/icon_dollar.svg';
+import BitcoinInput from 'src/assets/images/svgs/btc_input.svg';
+
 import HeaderTitle from 'src/components/HeaderTitle';
 import { RFValue } from 'react-native-responsive-fontsize';
 import { ScaledSheet } from 'react-native-size-matters';
@@ -19,24 +20,24 @@ import { useDispatch } from 'react-redux';
 import { useNavigation } from '@react-navigation/native';
 import useToastMessage from 'src/hooks/useToastMessage';
 import WalletDetails from './WalletDetails';
+import { TransferType } from 'src/common/data/enums/TransferType';
+import { Vault } from 'src/core/wallets/interfaces/vault';
 
 function AddSendAmount({ route }) {
   const navigation = useNavigation();
   const dispatch = useDispatch();
   const {
-    wallet,
+    sender,
+    recipient,
     address,
     amount: prefillAmount,
-    availableAmt,
-    walletName,
-    from,
+    transferType,
   }: {
-    wallet: Wallet;
+    sender: Wallet | Vault;
+    recipient: Wallet | Vault;
     address: string;
     amount: string;
-    availableAmt: string;
-    walletName: string;
-    from: string;
+    transferType: TransferType;
   } = route.params;
 
   const [amount, setAmount] = useState(prefillAmount || '');
@@ -45,17 +46,20 @@ function AddSendAmount({ route }) {
   const sendPhaseOneState = useAppSelector((state) => state.sendAndReceive.sendPhaseOne);
 
   useEffect(() => {
-    const confirmBalance = wallet.specs.balances.confirmed;
+    const confirmBalance = sender.specs.balances.confirmed;
     if (sendMaxFee && confirmBalance) {
       const sendMaxBalance = confirmBalance - sendMaxFee;
       setAmount(`${sendMaxBalance}`);
     }
   }, [sendMaxFee]);
 
-  const navigateToNext = (recipients) => {
+  const navigateToNext = () => {
     navigation.navigate('SendConfirmation', {
-      wallet,
-      recipients,
+      sender,
+      recipient,
+      address,
+      amount: parseInt(amount),
+      transferType,
     });
   };
   const { showToast } = useToastMessage();
@@ -68,7 +72,7 @@ function AddSendAmount({ route }) {
     });
     dispatch(
       sendPhaseOne({
-        wallet,
+        wallet: sender,
         recipients,
       })
     );
@@ -76,12 +80,7 @@ function AddSendAmount({ route }) {
 
   useEffect(() => {
     if (sendPhaseOneState.isSuccessful) {
-      const recipients = [];
-      recipients.push({
-        address,
-        amount: parseInt(amount),
-      });
-      navigateToNext(recipients);
+      navigateToNext();
     } else if (sendPhaseOneState.hasFailed) {
       if (sendPhaseOneState.failedErrorMessage === 'Insufficient balance')
         showToast('You have insufficient balance at this time.', null, 1000);
@@ -99,71 +98,22 @@ function AddSendAmount({ route }) {
   return (
     <ScreenWrapper>
       <HeaderTitle
-        title={from == 'Wallet' ? `Sending to Wallet` : `Enter the amount`}
+        title={
+          transferType == TransferType.WALLET_TO_WALLET ? `Sending to Wallet` : `Enter the amount`
+        }
         // subtitle={`Sending to ${address}`}
       />
-      {/* <Box
-        flexDirection={'row'}
-        alignItems={'center'}
-        justifyContent={'space-between'}
-        marginX={8}
-        marginY={windowHeight >= 850 ? '12' : windowHeight >= 750 ? '8' : '5'}
-      >
-        <Box flexDirection={'row'} alignItems={'center'}>
-          <Box
-            backgroundColor={'light.yellow1'}
-            height={10}
-            width={10}
-            borderRadius={20}
-            justifyContent={'center'}
-            alignItems={'center'}
-          >
-            <Text
-              color={'light.greenText'}
-              fontSize={20}
-              letterSpacing={1.12}
-              fontWeight={'extrabold'}
-            >
-              @
-            </Text>
-          </Box>
-          <Text
-            color={'light.greenText'}
-            fontSize={14}
-            letterSpacing={1.12}
-            fontWeight={300}
-            marginX={5}
-          >
-            {address}
-          </Text>
-        </Box>
-        <DollarInput />
-      </Box> */}
-
-      {/* { Transaction list} */}
       <Box
         style={{
           marginVertical: hp(5),
         }}
       >
-        <WalletDetails availableAmt={availableAmt} walletName={walletName} />
+        <WalletDetails
+          availableAmt={sender?.specs.balances.confirmed}
+          walletName={sender?.presentationData.name}
+        />
       </Box>
-      <Box
-      // style={{
-      //   marginTop: hp(32),
-      //   marginBottom: hp(32),
-      // }}
-      >
-        {/* <Transactions
-            transactions={[
-              {
-                address,
-                amount,
-              },
-            ]}
-            addTransaction={() => {}}
-          /> */}
-      </Box>
+
       <Box
         alignItems="center"
         style={{
@@ -187,7 +137,7 @@ function AddSendAmount({ route }) {
         >
           <Box flexDirection="row" alignItems="center">
             <Box marginRight={2}>
-              <DollarInput />
+              <BitcoinInput />
             </Box>
             <Box
               marginLeft={2}
@@ -197,7 +147,7 @@ function AddSendAmount({ route }) {
               height={7}
             />
             <Input
-              placeholder="Enter Amount"
+              placeholder="Enter Amount (sats)"
               placeholderTextColor="light.greenText"
               color="light.greenText"
               opacity={0.5}
@@ -213,13 +163,15 @@ function AddSendAmount({ route }) {
           </Box>
           <Pressable
             onPress={() => {
-              const confirmBalance = wallet.specs.balances.confirmed;
+              const confirmBalance = sender.specs.balances.confirmed;
               if (confirmBalance)
-                dispatch(calculateSendMaxFee({ numberOfRecipients: recipientCount, wallet }));
+                dispatch(
+                  calculateSendMaxFee({ numberOfRecipients: recipientCount, wallet: sender })
+                );
             }}
             style={{
               paddingHorizontal: hp(10),
-              paddingVertical: hp(3),
+              paddingVertical: hp(6),
               borderRadius: 5,
             }}
           >
@@ -238,7 +190,7 @@ function AddSendAmount({ route }) {
           justifyContent="center"
           alignItems="center"
         >
-          <TextInput placeholder="Add a note" style={styles.textInput} />
+          <TextInput placeholder="Add a note to self" style={styles.textInput} />
         </Box>
         <Box
           style={{
@@ -254,6 +206,7 @@ function AddSendAmount({ route }) {
                 navigation.goBack();
               }}
               primaryText="Send"
+              primaryDisable={Boolean(!amount)}
               primaryCallback={executeSendPhaseOne}
             />
           </Box>
