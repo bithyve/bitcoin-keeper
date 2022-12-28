c3e4618951fd39ef91d4bd6f77b1dc63f3f1d472
Pratyaksh Chandra <pratyaksh.chandra2898@gmail.com>
2022-12-28 09:42:38 +0530

    descriptor utils

---
+++ src/core/utils.ts
@@ -0,0 +1,37 @@
+import { VaultScheme, VaultSigner } from './wallets/interfaces/vault';
+
+export const getDerivationPath = (derivationPath: string) => {
+  return derivationPath.substring(2).split("'").join('h');
+};
+
+export const getMultiKeyExpressions = (signers: VaultSigner[]) => {
+  let keyExpressions = signers.map((signer: VaultSigner) => {
+    return getKeyExpression(signer.xpubInfo.xfp, signer.xpubInfo.derivationPath, signer.xpub);
+  });
+  return keyExpressions.join();
+};
+
+export const getKeyExpression = (
+  masterFingerprint: string,
+  derivationPath: string,
+  xpub: string
+) => {
+  return `[${masterFingerprint}/${getDerivationPath(derivationPath)}]${xpub}/<0;1>/*`;
+};
+
+export const genrateOutputDescriptors = (
+  isMultisig: boolean,
+  signers: VaultSigner[],
+  scheme: VaultScheme
+) => {
+  if (!isMultisig) {
+    const signer: VaultSigner = signers[0];
+    return `wpkh(${getKeyExpression(
+      signer.xpubInfo.xfp,
+      signer.xpubInfo.derivationPath,
+      signer.xpub
+    )})`;
+  } else {
+    return `sh(wsh(sortedmulti(${scheme.m},${getMultiKeyExpressions(signers)})))`;
+  }
+};
