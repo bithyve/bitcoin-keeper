84f3e173a79c7b69469c1be8559e912364be7602
Akshay Chavan <akshaychavan8551@gmail.com>
2023-01-05 15:31:34 +0530

    Done Transaction timing format modification according UX #1600

--- src/components/TransactionElement.tsx
+++ src/components/TransactionElement.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Box, useColorMode } from 'native-base';
+import { StyleSheet, TouchableOpacity } from 'react-native';
+import moment from 'moment';
 
 import { getAmount, getUnit } from 'src/common/constants/Bitcoin';
-import { getTransactionPadding, hp, wp } from 'src/common/data/responsiveness/responsive';
+import { hp, wp } from 'src/common/data/responsiveness/responsive';
 import { Transaction } from 'src/core/wallets/interfaces';
 
 import IconRecieve from 'src/assets/images/icon_received.svg';
 import IconSent from 'src/assets/images/icon_sent.svg';
 import BtcBlack from 'src/assets/images/btc_black.svg';
-import IconArrowGrey from 'src/assets/images/icon_arrow_grey.svg';
-import { StyleSheet, TouchableOpacity } from 'react-native';
+// import IconArrowGrey from 'src/assets/images/icon_arrow_grey.svg';
 import Text from 'src/components/KeeperText';
 
 function TransactionElement({
@@ -20,7 +21,7 @@ function TransactionElement({
   onPress?: () => void;
 }) {
   const { colorMode } = useColorMode();
-  const date = new Date(transaction?.date).toUTCString();
+  const date = moment(transaction?.date).format('DD MMM YY  •  hh:mma');
 
   return (
     <TouchableOpacity onPress={onPress}>
@@ -90,7 +91,7 @@ const styles = StyleSheet.create({
     fontWeight: '200',
     letterSpacing: 0.5,
     opacity: 0.82,
-    width: 120,
+    width: 125,
   },
   amountText: {
     fontSize: 19,
