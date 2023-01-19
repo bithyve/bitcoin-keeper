a242245a2ff95966cb3ee6540eb77dc905e1f34e
Raheel1258 <rahilahmad1258@gmail.com>
2023-01-19 13:46:15 +0500

    Read QR from image #1685

--- src/screens/QRScreens/ScanQR.tsx
+++ src/screens/QRScreens/ScanQR.tsx
@@ -2,6 +2,7 @@ import { ActivityIndicator, Dimensions, StyleSheet } from 'react-native';
 import Text from 'src/components/KeeperText';
 import { Box, HStack } from 'native-base';
 import React, { useContext, useEffect, useState } from 'react';
+import { QRreader } from "react-native-qr-decode-image-camera";
 
 import HeaderTitle from 'src/components/HeaderTitle';
 import { RNCamera } from 'react-native-camera';
@@ -11,14 +12,20 @@ import { decodeURBytes } from 'src/core/services/qr';
 import { useRoute } from '@react-navigation/native';
 import { LocalizationContext } from 'src/common/content/LocContext';
 import Note from 'src/components/Note/Note';
+import { ImageLibraryOptions, launchImageLibrary } from 'react-native-image-picker';
+import useToastMessage from 'src/hooks/useToastMessage';
+import UploadImage from 'src/components/UploadImage';
 
 const { width } = Dimensions.get('screen');
 let decoder = new URRegistryDecoder();
+
 function ScanQR() {
+
   const [qrPercent, setQrPercent] = useState(0);
   const [qrData, setData] = useState(0);
   const route = useRoute();
-  const { title = '', subtitle = '', onQrScan = () => {} } = route.params as any;
+  const { showToast } = useToastMessage();
+  const { title = '', subtitle = '', onQrScan = () => { } } = route.params as any;
 
   const { translations } = useContext(LocalizationContext);
   const { common } = translations;
@@ -54,6 +61,50 @@ function ScanQR() {
       }
     }
   };
+
+  const handleChooseImage = () => {
+    const options = {
+      quality: 1.0,
+      maxWidth: 500,
+      maxHeight: 500,
+      storageOptions: {
+        skipBackup: true,
+      },
+      mediaType: 'photo',
+    } as ImageLibraryOptions;
+
+    launchImageLibrary(options, async response => {
+      if (response.didCancel) {
+        return;
+      } else if (response.errorCode === 'camera_unavailable') {
+        showToast('Camera not available on device');
+        return;
+      } else if (response.errorCode === 'permission') {
+        showToast('Permission not satisfied');
+        return;
+      } else if (response.errorCode === 'others') {
+        showToast(response.errorMessage);
+        return;
+      } else {
+        QRreader(response.assets[0].uri)
+          .then(data => {
+            onBarCodeRead({
+              data
+            })
+          })
+          .catch(err => {
+            showToast('Invalid or No related QR code')
+          });
+      }
+    });
+  };
+
+  function enableImportImage(title: string): boolean {
+    if (title === 'Setting up SEEDSIGNER' || title === 'Setting up KEEPER') {
+      return true
+    }
+    return false
+  }
   return (
     <ScreenWrapper>
       <HeaderTitle
@@ -68,6 +119,7 @@ function ScanQR() {
           useNativeZoom
         />
       </Box>
+      {enableImportImage(title) && <UploadImage onPress={handleChooseImage} />}
       <HStack>
         {qrPercent !== 100 && <ActivityIndicator />}
         <Text>{`Scanned ${qrPercent}%`}</Text>
@@ -103,3 +155,4 @@ const styles = StyleSheet.create({
     padding: 20,
   },
 });
+
