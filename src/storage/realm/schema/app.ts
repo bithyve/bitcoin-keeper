38193be9e424d9b1fd60ca13eca271ce18ed4b92
Parsh <parsh.cosmos11@gmail.com>
2023-01-24 14:18:08 +0530

    schema updates: subscription, notification and backup

--- src/storage/realm/schema/app.ts
+++ src/storage/realm/schema/app.ts
@@ -1,28 +1,5 @@
 import { ObjectSchema } from 'realm';
-import { SubscriptionTier } from 'src/common/data/enums/SubscriptionTier';
 import { RealmSchema } from '../enum';
-
-export const StoreSubscriptionSchema: ObjectSchema = {
-  name: RealmSchema.StoreSubscription,
-  properties: {
-    name: {
-      type: 'string',
-      default: SubscriptionTier.L1,
-    },
-    productId: {
-      type: 'string',
-      default: SubscriptionTier.L1,
-    },
-    receipt: {
-      type: 'string?',
-      default: '',
-    },
-    level: {
-      type: 'int',
-      default: 0,
-    },
-  },
-};
 
 export const KeeperAppSchema: ObjectSchema = {
   name: RealmSchema.KeeperApp,
@@ -33,29 +10,15 @@ export const KeeperAppSchema: ObjectSchema = {
     primaryMnemonic: 'string',
     primarySeed: 'string',
     imageEncryptionKey: 'string',
+    networkType: 'string',
+    version: 'string',
     walletShellInstances: RealmSchema.WalletShellInstances,
     vaultShellInstances: RealmSchema.VaultShellInstances,
+    subscription: RealmSchema.StoreSubscription,
+    backup: RealmSchema.Backup,
     twoFADetails: `${RealmSchema.TwoFADetails}?`,
-    nodeConnect: `${RealmSchema.NodeConnect}?`,
     uai: `${RealmSchema.UAI}?`,
     notification: `${RealmSchema.Notification}?`,
-    subscription: {
-      type: RealmSchema.StoreSubscription,
-    },
-    version: 'string',
-    backupPassword: {
-      type: 'string?',
-      default: '',
-    },
-    backupPasswordHint: {
-      type: 'string?',
-      default: '',
-    },
-    backupMethod: {
-      type: 'string?',
-      default: '',
-    },
-    networkType: 'string',
   },
   primaryKey: 'id',
 };
