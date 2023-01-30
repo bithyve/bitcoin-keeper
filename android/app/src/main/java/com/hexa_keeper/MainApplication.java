ec7c25eec81dd8e39ed6c5468ac3eff1848d75b4
Shashank Shinde <shashank@cakesofttech.com>
2023-01-30 14:03:57 +0530

    minor

--- android/app/src/main/java/com/hexa_keeper/MainApplication.java
+++ android/app/src/main/java/com/hexa_keeper/MainApplication.java
@@ -27,9 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
-            packages.add(new GoogleDrivePackage());
+//            packages.add(new GoogleDrivePackage());
           return packages;
         }
 
