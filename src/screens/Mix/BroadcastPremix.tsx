2e34404b9aa1d9317650627efda1e3fb373f5659
Vivek <mail2vivek@pm.me>
2023-03-14 02:03:09 -0400

    -Cleaned up changes with real data

--- src/screens/Mix/BroadcastPremix.tsx
+++ src/screens/Mix/BroadcastPremix.tsx
@@ -1,9 +1,6 @@
-// create placeholder ui screen for utxo selection
-
 import { Box } from 'native-base';
 import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
 
 import HeaderTitle from 'src/components/HeaderTitle';
 import ScreenWrapper from 'src/components/ScreenWrapper';
@@ -13,147 +10,212 @@ import Text from 'src/components/KeeperText';
 import UtxoSummary from './UtxoSummary';
 import PageIndicator from 'src/components/PageIndicator';
 import KeeperModal from 'src/components/KeeperModal';
+import { useAppSelector } from 'src/store/hooks';
+import { SatsToBtc } from 'src/common/constants/Bitcoin';
 
-const utxos = [{ transactionId: 1, amount: 0.0001 },
-{ transactionId: 2, amount: 0.0001 },
-{ transactionId: 3, amount: 0.0001 },
-{ transactionId: 4, amount: 0.0001 },
-{ transactionId: 5, amount: 0.0001 },
-]
+const utxos = [
+  { transactionId: 1, amount: 0.0001 },
+  { transactionId: 2, amount: 0.0001 },
+  { transactionId: 3, amount: 0.0001 },
+  { transactionId: 4, amount: 0.0001 },
+  { transactionId: 5, amount: 0.0001 },
+];
 
 const broadcastModalContent = (onBroadcastModalCallback) => {
-    return (
-        <Box>
-            <Box>
-                <Text color="light.secondaryText" style={{ paddingTop: 5 }}><Text style={{ fontWeight: 'bold' }}>Premix</Text> This wallet contains the UTXOs from your premix transaction (also called the Tx0).
-                    Your premix transaction splits your UTXOs into equal amounts ready for mixing.</Text>
-                <Text color="light.secondaryText" style={{ paddingTop: 5 }}><Text style={{ fontWeight: 'bold' }}>Postmix</Text> This wallet contains the UTXOs from your mixes.
-                    Whirlpool will select UTXOs from both the Premix and the Postmix wallets to include in coinjoin transactions.
-                    Funds in this wallet can be considered mixed and are safe to spend anonymously, especially after a number of mixing rounds.</Text>
-                <Text color="light.secondaryText" style={{ paddingTop: 5 }}><Text style={{ fontWeight: 'bold' }}>Badbank</Text> This wallet contains the change from your premix (Tx0) transaction - whatever is left over from splitting your input UTXOs into equal amounts.
-                    Consider mixing any UTXOs here if they are large enough, but do not combine them with mixed funds</Text>
-            </Box>
-            <Box style={styles.modalFooter}>
-                <Buttons
-                    primaryText="Proceed"
-                    primaryCallback={() => onBroadcastModalCallback()}
-                />
-            </Box>
-        </Box>
-    )
-}
+  return (
+    <Box>
+      <Box>
+        <Text color="light.secondaryText" style={styles.premixInstructionText}>
+          <Text style={{ fontWeight: 'bold' }}>Premix</Text> This wallet contains the UTXOs from
+          your premix transaction (also called the Tx0). Your premix transaction splits your UTXOs
+          into equal amounts ready for mixing.
+        </Text>
+        <Text color="light.secondaryText" style={styles.premixInstructionText}>
+          <Text style={{ fontWeight: 'bold' }}>Postmix</Text> This wallet contains the UTXOs from
+          your mixes. Whirlpool will select UTXOs from both the Premix and the Postmix wallets to
+          include in coinjoin transactions. Funds in this wallet can be considered mixed and are
+          safe to spend anonymously, especially after a number of mixing rounds.
+        </Text>
+        <Text color="light.secondaryText" style={styles.premixInstructionText}>
+          <Text style={{ fontWeight: 'bold' }}>Badbank</Text> This wallet contains the change from
+          your premix (Tx0) transaction - whatever is left over from splitting your input UTXOs into
+          equal amounts. Consider mixing any UTXOs here if they are large enough, but do not combine
+          them with mixed funds.
+        </Text>
+      </Box>
+      <Box style={styles.modalFooter}>
+        <Buttons primaryText="Proceed" primaryCallback={() => onBroadcastModalCallback()} />
+      </Box>
+    </Box>
+  );
+};
 
-export default function BroadcastPremix() {
-    const navigation = useNavigation();
-    const [showBroadcastModal, setShowBroadcastModal] = useState(false);
+export default function BroadcastPremix({ route, navigation }) {
+  const { scode, fee, utxos, utxoCount, utxoTotal, pool, premixOutputCount } = route.params;
+  const [showBroadcastModal, setShowBroadcastModal] = useState(false);
+  const { satsEnabled } = useAppSelector((state) => state.settings);
+  const [premixOutputs, setPremixOutputs] = useState([]);
+  const [badbankChange, setBadbankChange] = useState(0);
 
-    useEffect(() => {
+  useEffect(() => {
+    setPremixOutputsAndBadbank();
+  }, []);
 
-    }, []);
-
-    const onBroadcastPremix = () => {
-        setShowBroadcastModal(true);
+  const setPremixOutputsAndBadbank = () => {
+    const outputs = [];
+    for (let i = 0; i < premixOutputCount; i++) {
+      outputs.push({ amount: pool.must_mix_balance_cap });
     }
-
-    const closeBroadcastModal = async () => {
-        setShowBroadcastModal(false);
-    };
-
-    const onBroadcastModalCallback = () => {
-        setShowBroadcastModal(false);
-        navigation.navigate('WalletDetails');
-    }
-
-    return (
-        <ScreenWrapper backgroundColor="light.mainBackground" barStyle="dark-content">
-            <HeaderTitle
-                paddingLeft={25}
-                title="Broadcast Premix Transaction"
-                subtitle="Your premix transaction is ready to be broadcast. Please review the details below and click the button to broadcast your transaction."
-            />
-            <UtxoSummary utxoCount={4} totalAmount={0.001} />
-            <Box style={styles.textArea}>
-                <Text color='#017963' style={styles.textWidth}>Whirlpool Fee</Text>
-                <Text color="light.secondaryText">5</Text>
-            </Box>
-            <Box style={styles.textArea}>
-                <Text color='#017963' style={styles.textWidth}>Badbank Change</Text>
-                <Text color="light.secondaryText">0.05</Text>
-            </Box>
-            {utxos && utxos.map((utxo, index) => {
-                return (
-                    <Box style={styles.textArea}>
-                        <Text color='#017963' style={styles.textWidth}>Premix #{index + 1}</Text>
-                        <Text color="light.secondaryText">{utxo.amount}</Text>
-                    </Box>
-                )
-            })
-            }
-            <Box style={styles.textArea}>
-                <Text color='#017963' style={styles.textWidth}>Fee</Text>
-                <Text color="light.secondaryText">5 Utxos</Text>
-            </Box>
-            <Box style={styles.footerContainer}>
-                <Box style={styles.border} borderColor="light.GreyText" />
-                <Box style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                    <Box style={{ alignSelf: 'center', paddingBottom: 4, paddingLeft: 20 }}><PageIndicator currentPage={2} totalPage={2} /></Box>
-                    <Box style={styles.footerItemContainer}>
-                        <Buttons
-                            primaryText="Broadcast Premix"
-                            primaryCallback={() => onBroadcastPremix()}
-                        />
-                    </Box>
-                </Box>
-            </Box>
-            <KeeperModal
-                justifyContent='flex-end'
-                visible={showBroadcastModal}
-                close={closeBroadcastModal}
-                title="Broadcast Premix"
-                subTitle="Initiating your first coinjoin will add three new wallets to your existing wallet: Premix, Postmix and Badbank."
-                subTitleColor="#5F6965"
-                modalBackground={['#F7F2EC', '#F7F2EC']}
-                buttonBackground={['#00836A', '#073E39']}
-                buttonText=""
-                buttonTextColor="#FAFAFA"
-                buttonCallback={closeBroadcastModal}
-                closeOnOverlayClick={false}
-                Content={() => broadcastModalContent(onBroadcastModalCallback)}
-            />
-        </ScreenWrapper>
+    setPremixOutputs(outputs);
+    setBadbankChange(
+      utxoTotal - fee.averageTxFee - pool.fee_value - pool.must_mix_balance_cap * premixOutputCount
     );
+  };
+
+  const onBroadcastPremix = () => {
+    setShowBroadcastModal(true);
+  };
+
+  const closeBroadcastModal = async () => {
+    setShowBroadcastModal(false);
+  };
+
+  const onBroadcastModalCallback = () => {
+    setShowBroadcastModal(false);
+    navigation.navigate('WalletDetails');
+  };
+
+  const valueByPreferredUnit = (value) => {
+    if (!value) return '';
+    const valueInPreferredUnit = satsEnabled ? value : SatsToBtc(value);
+    return valueInPreferredUnit;
+  };
+
+  const getPreferredUnit = () => {
+    return satsEnabled ? 'sats' : 'btc';
+  };
+
+  return (
+    <ScreenWrapper backgroundColor="light.mainBackground" barStyle="dark-content">
+      <HeaderTitle
+        paddingLeft={25}
+        title="Broadcast Premix Transaction"
+        subtitle="Your premix transaction is ready to be broadcast. Please review the details below and click the button to broadcast your transaction."
+      />
+      <UtxoSummary utxoCount={utxoCount} totalAmount={utxoTotal} />
+      <Box style={styles.textArea}>
+        <Text color="#017963" style={styles.textWidth}>
+          Whirlpool Fee
+        </Text>
+        <Box style={styles.textDirection}>
+          <Text color="light.secondaryText">{valueByPreferredUnit(pool?.fee_value)}</Text>
+          <Text color="light.secondaryText" style={{ paddingLeft: 5 }}>
+            {getPreferredUnit()}
+          </Text>
+        </Box>
+      </Box>
+      <Box style={styles.textArea}>
+        <Text color="#017963" style={styles.textWidth}>
+          Badbank Change
+        </Text>
+        <Box style={styles.textDirection}>
+          <Text color="light.secondaryText">{valueByPreferredUnit(badbankChange)}</Text>
+          <Text color="light.secondaryText" style={{ paddingLeft: 5 }}>
+            {getPreferredUnit()}
+          </Text>
+        </Box>
+      </Box>
+      {premixOutputs &&
+        premixOutputs.map((output, index) => {
+          return (
+            <Box style={styles.textArea}>
+              <Text color="#017963" style={styles.textWidth}>
+                Premix #{index + 1}
+              </Text>
+              <Box style={styles.textDirection}>
+                <Text color="light.secondaryText">{valueByPreferredUnit(output.amount)}</Text>
+                <Text color="light.secondaryText" style={{ paddingLeft: 5 }}>
+                  {getPreferredUnit()}
+                </Text>
+              </Box>
+            </Box>
+          );
+        })}
+      <Box style={styles.textArea}>
+        <Text color="#017963" style={styles.textWidth}>
+          Fee
+        </Text>
+        <Box style={styles.textDirection}>
+          <Text color="light.secondaryText">{valueByPreferredUnit(fee?.averageTxFee)}</Text>
+          <Text color="light.secondaryText" style={{ paddingLeft: 5 }}>
+            {getPreferredUnit()}
+          </Text>
+        </Box>
+      </Box>
+      <Box style={styles.footerContainer}>
+        <Box style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+          <Box style={{ alignSelf: 'center', paddingBottom: 4, paddingLeft: 20 }}>
+            <PageIndicator currentPage={2} totalPage={2} />
+          </Box>
+          <Box style={styles.footerItemContainer}>
+            <Buttons primaryText="Broadcast Premix" primaryCallback={() => onBroadcastPremix()} />
+          </Box>
+        </Box>
+      </Box>
+      <KeeperModal
+        justifyContent="flex-end"
+        visible={showBroadcastModal}
+        close={closeBroadcastModal}
+        title="Broadcast Premix"
+        subTitle="Initiating your first coinjoin will add three new wallets to your existing wallet: Premix, Postmix and Badbank."
+        subTitleColor="#5F6965"
+        modalBackground={['#F7F2EC', '#F7F2EC']}
+        buttonBackground={['#00836A', '#073E39']}
+        buttonText=""
+        buttonTextColor="#FAFAFA"
+        buttonCallback={closeBroadcastModal}
+        closeOnOverlayClick={false}
+        Content={() => broadcastModalContent(onBroadcastModalCallback)}
+      />
+    </ScreenWrapper>
+  );
 }
 
 const styles = StyleSheet.create({
-    textArea: {
-        marginTop: 20,
-        marginLeft: 50,
-        flexDirection: 'row',
-    },
-    footerContainer: {
-        position: 'absolute',
-        bottom: 0,
-        width: wp(375),
-        paddingHorizontal: 5,
-    },
-    border: {
-        borderWidth: 0.5,
-        borderRadius: 20,
-        opacity: 0.2,
-    },
-    footerItemContainer: {
-        flexDirection: 'row',
-        marginTop: windowHeight > 800 ? 5 : 5,
-        marginBottom: windowHeight > 800 ? hp(10) : 0,
-        paddingBottom: 15,
-        justifyContent: 'flex-end',
-        marginHorizontal: 16,
-    },
-    modalFooter: {
-        width: '100%',
-        flexDirection: 'row',
-        justifyContent: 'flex-end',
-        marginTop: 10,
-        marginBottom: 10,
-    }
+  textArea: {
+    marginTop: 20,
+    marginLeft: 50,
+    flexDirection: 'row',
+  },
+  textWidth: {
+    width: '45%',
+  },
+  textDirection: {
+    flexDirection: 'row',
+  },
+  footerContainer: {
+    position: 'absolute',
+    bottom: 0,
+    width: wp(375),
+    paddingHorizontal: 5,
+  },
+  footerItemContainer: {
+    flexDirection: 'row',
+    marginTop: windowHeight > 800 ? 5 : 5,
+    marginBottom: windowHeight > 800 ? hp(10) : 0,
+    paddingBottom: 15,
+    justifyContent: 'flex-end',
+    marginHorizontal: 16,
+  },
+  premixInstructionText: {
+    fontSize: 12,
+    paddingTop: 10,
+  },
+  modalFooter: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    marginTop: 10,
+    marginBottom: 10,
+  },
 });
--- src/screens/Mix/UtxoSummary.tsx
+++ src/screens/Mix/UtxoSummary.tsx
@@ -6,44 +6,51 @@ import { StyleSheet } from 'react-native';
 import { Box, Text } from 'native-base';
 import BtcInput from 'src/assets/images/btc_input.svg';
 import WalletIcon from 'src/assets/images/wallet_color.svg';
+import { useAppSelector } from 'src/store/hooks';
+import { SatsToBtc } from 'src/common/constants/Bitcoin';
 
 export default function UtxoSummary({ utxoCount, totalAmount }) {
-    return (
-        <Box style={styles.utxo}>
-            <WalletIcon />
-            <Box style={styles.utxoSummary}>
-                <Box style={styles.utxoTextDirection}>
-                    <Text style={styles.noOfUtxo}>{utxoCount} </Text>
-                    <Text color="light.secondaryText">Utxo's Selected</Text>
-                </Box>
-                <Box style={styles.sats}>
-                    <BtcInput />
-                    <Text color="light.secondaryText"> {totalAmount}</Text>
-                </Box>
-            </Box>
+  const { satsEnabled } = useAppSelector((state) => state.settings);
+
+  return (
+    <Box style={styles.utxo}>
+      <WalletIcon />
+      <Box style={styles.utxoSummary}>
+        <Box style={styles.utxoTextDirection}>
+          <Text style={styles.noOfUtxo}>{utxoCount} </Text>
+          <Text color="light.secondaryText">UTXOs Selected</Text>
         </Box>
-    )
+        <Box style={styles.sats}>
+          <BtcInput />
+          <Text color="light.secondaryText">
+            {' '}
+            {satsEnabled ? totalAmount : SatsToBtc(totalAmount)}
+          </Text>
+        </Box>
+      </Box>
+    </Box>
+  );
 }
 
 const styles = StyleSheet.create({
-    utxo: {
-        flexDirection: 'row',
-        paddingHorizontal: 25,
-        marginTop: 20,
-        marginLeft: 20
-    },
-    utxoSummary: {
-        marginTop: 10,
-    },
-    noOfUtxo: {
-        fontSize: 15,
-        fontWeight: 'bold'
-    },
-    utxoTextDirection: {
-        flexDirection: 'row',
-    },
-    sats: {
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
+  utxo: {
+    flexDirection: 'row',
+    paddingHorizontal: 25,
+    marginTop: 20,
+    marginLeft: 20,
+  },
+  utxoSummary: {
+    marginTop: 10,
+  },
+  noOfUtxo: {
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+  utxoTextDirection: {
+    flexDirection: 'row',
+  },
+  sats: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
 });
