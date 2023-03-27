c9ef532a31d471ab84e2b130fc2f6314ce73b706
Raheel1258 <rahilahmad1258@gmail.com>
2023-03-27 20:29:03 +0500

    Add Account Tabs

--- src/screens/UTXOManagement/UTXOManagement.tsx
+++ src/screens/UTXOManagement/UTXOManagement.tsx
@@ -13,102 +13,107 @@ import Text from 'src/components/KeeperText';
 import { hp, wp } from 'src/common/data/responsiveness/responsive';
 import { StyleSheet } from 'react-native';
 import UTXOSelectionTotal from 'src/components/UTXOsComponents/UTXOSelectionTotal';
+import { AccountSelectionTab, AccountTypes } from 'src/components/AccountSelectionTab';
 
 function Footer({ vault, setEnableSelection, enableSelection, selectedUTXOs }) {
-    return enableSelection ? (
-        <FinalizeFooter
-            currentWallet={vault}
-            selectedUTXOs={selectedUTXOs}
-            setEnableSelection={setEnableSelection}
-        />
-    ) : (
-        <UTXOFooter setEnableSelection={setEnableSelection} enableSelection={enableSelection} />
-    );
+  return enableSelection ? (
+    <FinalizeFooter
+      currentWallet={vault}
+      selectedUTXOs={selectedUTXOs}
+      setEnableSelection={setEnableSelection}
+    />
+  ) : (
+    <UTXOFooter setEnableSelection={setEnableSelection} enableSelection={enableSelection} />
+  );
 }
 function UTXOManagement({ route }) {
-    const styles = getStyles();
-    const { data, routeName } = route.params || {};
-    const [enableSelection, _setEnableSelection] = useState(false);
-    const { confirmedUTXOs, unconfirmedUTXOs } = data?.specs || {
-        confirmedUTXOs: [],
-        unconfirmedUTXOs: [],
-    };
-    const utxos =
-        confirmedUTXOs
-            .map((utxo) => {
-                utxo.confirmed = true;
-                return utxo;
-            })
-            .concat(
-                unconfirmedUTXOs.map((utxo) => {
-                    utxo.confirmed = false;
-                    return utxo;
-                })
-            ) || [];
+  const styles = getStyles();
+  const { data, routeName } = route.params || {};
+  const [enableSelection, _setEnableSelection] = useState(false);
+  const { confirmedUTXOs, unconfirmedUTXOs } = data?.specs || {
+    confirmedUTXOs: [],
+    unconfirmedUTXOs: [],
+  };
+  const utxos =
+    confirmedUTXOs
+      .map((utxo) => {
+        utxo.confirmed = true;
+        return utxo;
+      })
+      .concat(
+        unconfirmedUTXOs.map((utxo) => {
+          utxo.confirmed = false;
+          return utxo;
+        })
+      ) || [];
 
-    const [selectionTotal, setSelectionTotal] = useState(0);
-    const [selectedUTXOMap, setSelectedUTXOMap] = useState({});
-    const selectedUTXOs = utxos.filter((utxo) => selectedUTXOMap[`${utxo.txId}${utxo.vout}`]);
+  const [selectionTotal, setSelectionTotal] = useState(0);
+  const [selectedUTXOMap, setSelectedUTXOMap] = useState({});
+  const selectedUTXOs = utxos.filter((utxo) => selectedUTXOMap[`${utxo.txId}${utxo.vout}`]);
 
-    const cleanUp = useCallback(() => {
-        setSelectedUTXOMap({});
-        setSelectionTotal(0);
-    }, []);
+  const [selectedAccount, setSelectedAccount] = useState<AccountTypes>(AccountTypes.DEPOSIT)
 
-    const setEnableSelection = useCallback(
-        (value) => {
-            _setEnableSelection(value);
-            if (!value) {
-                cleanUp();
-            }
-        },
-        [cleanUp]
-    );
-    return (
-        <ScreenWrapper>
-            <HeaderTitle learnMore />
-            <Box style={styles.dailySpendingWrapper}>
-                <HStack>
-                    <Box paddingRight={3}>
-                        {routeName === 'Vault' ? <VaultIcon /> : <LinkedWallet />}
-                    </Box>
-                    <VStack>
-                        <Text color="light.greenText" style={[styles.vaultInfoText, { fontSize: 16 }]} >
-                            Daily Spending
-                        </Text>
-                        <Text color="light.grayText" style={[styles.vaultInfoText, { fontSize: 12 }]}>
-                            Lorem ipsum dolor sit amet
-                        </Text>
-                    </VStack>
-                </HStack>
-            </Box>
-            <Box style={{ height: '66%' }}>
-                {Object.values(selectedUTXOMap).length ?
-                    <UTXOSelectionTotal selectionTotal={selectionTotal} selectedUTXOs={selectedUTXOs} /> : null
-                }
-                <UTXOList
-                    utxoState={utxos}
-                    enableSelection={enableSelection}
-                    setSelectionTotal={setSelectionTotal}
-                    selectedUTXOMap={selectedUTXOMap}
-                    setSelectedUTXOMap={setSelectedUTXOMap}
-                    currentWallet={data}
-                    emptyIcon={routeName === 'Vault' ? NoVaultTransactionIcon : NoTransactionIcon}
-                />
-            </Box>
-            <Footer vault={data} setEnableSelection={setEnableSelection} enableSelection={enableSelection} selectedUTXOs={selectedUTXOs} />
-        </ScreenWrapper>
-    )
+  const cleanUp = useCallback(() => {
+    setSelectedUTXOMap({});
+    setSelectionTotal(0);
+  }, []);
+
+  const setEnableSelection = useCallback(
+    (value) => {
+      _setEnableSelection(value);
+      if (!value) {
+        cleanUp();
+      }
+    },
+    [cleanUp]
+  );
+  return (
+    <ScreenWrapper>
+      <HeaderTitle learnMore />
+      <Box style={styles.dailySpendingWrapper}>
+        <HStack>
+          <Box paddingRight={3}>
+            {routeName === 'Vault' ? <VaultIcon /> : <LinkedWallet />}
+          </Box>
+          <VStack>
+            <Text color="light.greenText" style={[styles.vaultInfoText, { fontSize: 16 }]} >
+              Daily Spending
+            </Text>
+            <Text color="light.grayText" style={[styles.vaultInfoText, { fontSize: 12 }]}>
+              Lorem ipsum dolor sit amet
+            </Text>
+          </VStack>
+        </HStack>
+        {/* this is account switch tab */}
+        {/* <AccountSelectionTab selectedAccount={selectedAccount} setSelectedAccount={setSelectedAccount} /> */}
+      </Box>
+      <Box style={{ height: '66%' }}>
+        {Object.values(selectedUTXOMap).length ?
+          <UTXOSelectionTotal selectionTotal={selectionTotal} selectedUTXOs={selectedUTXOs} /> : null
+        }
+        <UTXOList
+          utxoState={utxos}
+          enableSelection={enableSelection}
+          setSelectionTotal={setSelectionTotal}
+          selectedUTXOMap={selectedUTXOMap}
+          setSelectedUTXOMap={setSelectedUTXOMap}
+          currentWallet={data}
+          emptyIcon={NoVaultTransactionIcon}
+        />
+      </Box>
+      <Footer vault={data} setEnableSelection={setEnableSelection} enableSelection={enableSelection} selectedUTXOs={selectedUTXOs} />
+    </ScreenWrapper>
+  )
 }
 const getStyles = () =>
-    StyleSheet.create({
-        vaultInfoText: {
-            marginLeft: wp(3),
-            letterSpacing: 1.28,
-        },
-        dailySpendingWrapper: {
-            marginLeft: wp(20),
-            marginVertical: hp(20)
-        }
-    });
+  StyleSheet.create({
+    vaultInfoText: {
+      marginLeft: wp(3),
+      letterSpacing: 1.28,
+    },
+    dailySpendingWrapper: {
+      marginLeft: wp(20),
+      marginVertical: hp(20)
+    }
+  });
 export default UTXOManagement
