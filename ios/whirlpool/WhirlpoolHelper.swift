2fa1d041fc12c400993937890cae6fa8be49df98
PraneethGunas <praneethgunas404@gmail.com>
2023-04-05 12:25:23 +0530

    Bridging iOS whirlpool client with native c_type casting

--- ios/whirlpool/WhirlpoolHelper.swift
+++ ios/whirlpool/WhirlpoolHelper.swift
@@ -17,7 +17,7 @@ import Foundation
     let str =  String(cString: result!)
     callback(str)
   }
-  
+
   @objc func initiateClient(callback: @escaping ((String)-> Void)){
     let result = initiate()
     let str =  String(cString: result!)
@@ -32,6 +32,24 @@ import Foundation
   
   @objc func getTx0Data(callback: @escaping ((String)-> Void)){
     let result = gettx0data()
+    let str =  String(cString: result!)
+    callback(str)
+  }
+  
+  @objc func tx0Preview(inputs_value: Int32, pool_str: String, premix_fee_per_byte: Double, fees_address:String, input_structure_str: String, miner_fee_per_byte: Int32, coordinator_fee: Int32, n_wanted_max_outputs_str: String, n_pool_max_outputs: Int32, callback: @escaping ((String) -> Void)){
+    let result = tx0_preview(inputs_value, pool_str, premix_fee_per_byte,fees_address, input_structure_str, miner_fee_per_byte, coordinator_fee, n_wanted_max_outputs_str, n_pool_max_outputs)
+    let str =  String(cString: result!)
+    callback(str)
+  }
+  
+  @objc func intoPsbt(preview_str:String, tx0_data_str:String, inputs_str:String, address_bank_str:String, change_addr_str:String, callback: @escaping ((String)-> Void)){
+    let result = into_psbt(preview_str, tx0_data_str, inputs_str, address_bank_str, change_addr_str)
+    let str =  String(cString: result!)
+    callback(str)
+  }
+  
+  @objc func tx0push(tx_str:String, pool_id_str:String, callback: @escaping ((String)-> Void)){
+    let result = tx0_push(tx_str, pool_id_str)
     let str =  String(cString: result!)
     callback(str)
   }
