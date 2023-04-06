afe97d576665da7bc709a4440e221227909912f8
Shashank Shinde <shashank@cakesofttech.com>
2023-04-06 16:58:54 +0530

    android whirpool methods updated

--- android/app/src/main/java/com/hexa_keeper/WhirlpoolBridge.java
+++ android/app/src/main/java/com/hexa_keeper/WhirlpoolBridge.java
@@ -9,7 +9,7 @@ public class WhirlpoolBridge {
     public static native String initiate();
     public static native String pools();
     public static native String gettx0data();
-    public static native String tx0preview(Integer inputsValue, String poolStr, Integer premixFeePerByte, String inputStructureStr,Integer minerFeePerByte,Integer coordinatorFee, String nWantedMaxOutputsStr, Integer nPoolMaxOutputs);
+    public static native String tx0preview(String inputsValue, String poolStr, String premixFeePerByte, String feesAddress, String inputStructureStr,String minerFeePerByte,String coordinatorFee, String nWantedMaxOutputsStr, String nPoolMaxOutputs);
     public static native String tx0push(String txStr, String poolIdStr);
     public static native String intopsbt(String previewStr, String tx0DataStr, String inputsStr,String addressBankStr, String changeAddrStr);
     public static native String blocking(String input, String privateKey, String destination,String poolId, String denomination, String preUserHash, String network, String blockHeight);
--- src/nativemodules/WhirlpoolServices.tsx
+++ src/nativemodules/WhirlpoolServices.tsx
@@ -63,15 +63,15 @@ export default class WhirlpoolServices {
   ): Promise<Preview> => {
     try {
       const result = await Whirlpool.tx0Preview(
-        inputsValue,
+        `${inputsValue}`,
         JSON.stringify(pool),
         feeAddress,
         JSON.stringify(inputStructure),
-        minerFeePerByte,
-        coordinatorFee,
+        `${minerFeePerByte}`,
+        `${coordinatorFee}`,
         nWantedMaxOutputsStr,
-        nPoolMaxOutputs,
-        premixFeePerByte
+        `${nPoolMaxOutputs}`,
+        `${premixFeePerByte}`
       );
       if (!result) throw new Error('Failed to generate tx0 preview');
       if (result === 'No enough sats for mixing.') {
@@ -124,7 +124,7 @@ export default class WhirlpoolServices {
    */
   static tx0Push = async (txHex: string, poolId: string): Promise<string> => {
     try {
-      const result = await Whirlpool.tx0push(txHex, poolId);
+      const result = await Whirlpool.tx0Push(txHex, poolId);
       if (!result) throw new Error('Failed to broadcast tx0');
       return result;
     } catch (error) {
