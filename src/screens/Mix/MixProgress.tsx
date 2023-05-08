53ac071c72424755fe1688c44def3a9c84e4ede8
Shashank Shinde <shashank@cakesofttech.com>
2023-05-08 11:40:43 +0530

    keep screen awake while mixing

--- src/screens/Mix/MixProgress.tsx
+++ src/screens/Mix/MixProgress.tsx
@@ -36,6 +36,7 @@ import { captureError } from 'src/core/services/sentry';
 import useWhirlpoolWallets from 'src/hooks/useWhirlpoolWallets';
 import { initiateWhirlpoolSocket } from 'src/core/services/whirlpool/sockets';
 import { io } from 'src/core/services/channel';
+import KeepAwake from 'src/nativemodules/KeepScreenAwake';
 
 const getBackgroungColor = (completed: boolean, error: boolean): string => {
   if (error) {
@@ -166,6 +167,11 @@ function MixProgress({
 
   useEffect(() => {
     getPoolsData();
+    KeepAwake.activate()
+
+    return () => {
+      KeepAwake.deactivate()
+    }
   }, []);
 
   useEffect(() => {
