7d02ee2873eaf746c0c704326e615a9f7ac347a3
Pratyaksh Chandra <pratyaksh.chandra2898@gmail.com>
2023-05-11 14:54:39 +0530

    HeaderDetailsComponent

--- src/screens/NewHomeScreen/VaultScreen.tsx
+++ src/screens/NewHomeScreen/VaultScreen.tsx
@@ -1,11 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import ScreenWrapper from 'src/components/ScreenWrapper';
+import HeaderDetails from './components/HeaderDetails';
 
 const VaultScreen = () => {
   return (
     <ScreenWrapper>
-      <Text>VaultScreen</Text>
+      <HeaderDetails />
+      <Text>Vaults Screen</Text>
     </ScreenWrapper>
   );
 };
