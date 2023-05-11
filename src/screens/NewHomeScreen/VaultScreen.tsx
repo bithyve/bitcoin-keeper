4a805d85435da9285bfbdceb2272c693a36d1529
Akshay Chavan <akshaychavan8551@gmail.com>
2023-05-11 18:28:16 +0530

    added back color

--- src/screens/NewHomeScreen/VaultScreen.tsx
+++ src/screens/NewHomeScreen/VaultScreen.tsx
@@ -9,44 +9,46 @@ import BitcoinIcon from 'src/assets/images/icon_bitcoin_white.svg'
 import { hp } from 'src/common/data/responsiveness/responsive';
 import Text from 'src/components/KeeperText';
 import HeaderDetails from './components/HeaderDetails';
-import ListItemView from './components/HeaderDetails/components/ListItemView';
+import ListItemView from './components/ListItemView';
 
 function VaultScreen() {
   return (
-    <ScreenWrapper>
+    <Box style={{ flex: 1 }}>
       <HeaderDetails />
-      <Box style={styles.hideBalanceWrapper}>
-        <HideIcon />
-        <Text style={styles.hideBalanceText}>&nbsp;&nbsp;HIDE BALANCES</Text>
-      </Box>
-      <Box style={styles.titleWrapper}>
-        <Text style={styles.titleText} color='light.primaryText'>Your Vault</Text>
-        <Text style={styles.subTitleText} color='light.secondaryText'>Beach and sunshine baby!</Text>
-      </Box>
-      <Box style={styles.vaultDetailsWrapper} backgroundColor='light.learnMoreBorder'>
-        <Box style={styles.signingDeviceWrapper}>
-          <Box style={styles.signingDeviceDetails}>
-            <Text style={styles.signingDeviceText} color='light.white'>3 of 5 Vault</Text>
-            <Box style={styles.signingDeviceList}>
-              <ColcardIcon />
-              <ColcardIcon />
-              <ColcardIcon />
-              <ColcardIcon />
+      <ScreenWrapper>
+        <Box style={styles.hideBalanceWrapper}>
+          <HideIcon />
+          <Text style={styles.hideBalanceText}>&nbsp;&nbsp;HIDE BALANCES</Text>
+        </Box>
+        <Box style={styles.titleWrapper}>
+          <Text style={styles.titleText} color='light.primaryText'>Your Vault</Text>
+          <Text style={styles.subTitleText} color='light.secondaryText'>Beach and sunshine baby!</Text>
+        </Box>
+        <Box style={styles.vaultDetailsWrapper} backgroundColor='light.learnMoreBorder'>
+          <Box style={styles.signingDeviceWrapper}>
+            <Box style={styles.signingDeviceDetails}>
+              <Text style={styles.signingDeviceText} color='light.white'>3 of 5 Vault</Text>
+              <Box style={styles.signingDeviceList}>
+                <ColcardIcon />
+                <ColcardIcon />
+                <ColcardIcon />
+                <ColcardIcon />
+              </Box>
+            </Box>
+            <Box style={styles.unConfirmBalanceView}>
+              <Text style={styles.unconfirmText} color='light.white'>Unconfirmed</Text>
+              <Text style={styles.balanceText} color='light.white'>₿ 0.00050</Text>
             </Box>
           </Box>
-          <Box style={styles.unConfirmBalanceView}>
-            <Text style={styles.unconfirmText} color='light.white'>Unconfirmed</Text>
-            <Text style={styles.balanceText} color='light.white'>₿ 0.00050</Text>
+          <Box style={styles.availableBalanceWrapper}>
+            <Text style={styles.availableText} color='light.white'>Available Balance</Text>
+            <Text style={styles.availablebalanceText} color='light.white'>₿ 10.0006</Text>
           </Box>
         </Box>
-        <Box style={styles.availableBalanceWrapper}>
-          <Text style={styles.availableText} color='light.white'>Available Balance</Text>
-          <Text style={styles.availablebalanceText} color='light.white'>₿ 10.0006</Text>
-        </Box>
-      </Box>
-      <ListItemView icon={<InheritanceIcon />} title='Inheritance' subTitle='Setup inheritance Key' />
-      <ListItemView icon={<BitcoinIcon />} title='Buy' subTitle='Stack sats directly in the vault' />
-    </ScreenWrapper>
+        <ListItemView icon={<InheritanceIcon />} title='Inheritance' subTitle='Setup inheritance Key' />
+        <ListItemView icon={<BitcoinIcon />} title='Buy' subTitle='Stack sats directly in the vault' />
+      </ScreenWrapper>
+    </Box>
   );
 };
 
