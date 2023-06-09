35e6a115b5ce7e592b14e116a11f5b808042b905
Akshay Chavan <akshaychavan8551@gmail.com>
2023-06-09 14:11:08 +0530

    Added - Keep your signing devices safe Modal - #2632

--- src/common/data/defaultData/defaultData.tsx
+++ src/common/data/defaultData/defaultData.tsx
@@ -1,23 +1,24 @@
 import SecutityTip from 'src/assets/images/securityTip.svg';
 import WhirlpoolLoader from 'src/components/WhirlpoolLoader';
+import SigningDeviceSafe from 'src/assets/images/signingDeviceSafe.svg'
 import React from 'react';
 
 export const securityTips = [
-  {
-    title: 'Security Tip',
-    subTitle:
-      'Check the send-to address on a signing device you are going to use to sign the transaction',
-    assert: <SecutityTip />,
-    message:
-      'This ensures that the signed transaction has the intended recipient and the address was not swapped',
-  },
-  {
-    title: 'Security Tip',
-    subTitle:
-      'Devices with Register Vault tag provide additional checks when you are sending funds from your Vault',
-    assert: <SecutityTip />,
-    message: 'These provide additional security checks when you make an outgoing transaction',
-  },
+  // {
+  //   title: 'Security Tip',
+  //   subTitle:
+  //     'Check the send-to address on a signing device you are going to use to sign the transaction',
+  //   assert: <SecutityTip />,
+  //   message:
+  //     'This ensures that the signed transaction has the intended recipient and the address was not swapped',
+  // },
+  // {
+  //   title: 'Security Tip',
+  //   subTitle:
+  //     'Devices with Register Vault tag provide additional checks when you are sending funds from your Vault',
+  //   assert: <SecutityTip />,
+  //   message: 'These provide additional security checks when you make an outgoing transaction',
+  // },
   {
     title: 'Introducing Whirlpool',
     subTitle: 'Whirlpool gives you forward looking privacy by breaking deterministic links of your future transactions from past ones',
@@ -31,17 +32,23 @@ export const securityTips = [
     assert: <SecutityTip />,
     message: 'This will mean that the funds are received at the correct address',
   },
+  // {
+  //   title: 'Security Tip',
+  //   subTitle:
+  //     'Recreate the Vault on another coordinator software and check if the multisig has the same details',
+  //   assert: <SecutityTip />,
+  //   message: 'This is a way to minimise the trust you have to have on Keeper',
+  // },
   {
-    title: 'Security Tip',
-    subTitle:
-      'Recreate the Vault on another coordinator software and check if the multisig has the same details',
-    assert: <SecutityTip />,
-    message: 'This is a way to minimise the trust you have to have on Keeper',
+    title: 'Keep your signing devices safe',
+    subTitle: 'Signing devices are what control your funds.',
+    assert: <SigningDeviceSafe />,
+    message: 'These are generally offline and to keep them secure is your responsibility. ',
   },
   {
     title: 'Security Tip',
     subTitle:
-      'Recreate the multisig on more coordinators. Receive a small amount and send a part of it. Check whether the balances are appropriately reflected across all the coordinators after each step',
+      'Recreate the multisig Vault on more coordinators. Receive a small amount and send a part of it. Check whether the balances are appropriately reflected across all the coordinators after each step',
     assert: <SecutityTip />,
     message: 'Testing out your setup before using it is always a good idea',
   },
