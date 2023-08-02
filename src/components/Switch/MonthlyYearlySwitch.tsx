4a35747dfe71bd428ba9faf020c46982d28512ec
akshay8551 <akshaychavan8551@gmail.com>
2023-08-02 13:45:44 +0530

    Choose Plan - Need Design variation for Dark mode #3121

--- src/components/Switch/MonthlyYearlySwitch.tsx
+++ src/components/Switch/MonthlyYearlySwitch.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { TouchableOpacity, StyleSheet } from 'react-native';
-import { Box } from 'native-base';
+import { Box, useColorMode } from 'native-base';
 import Text from '../KeeperText';
 import KeeperGradient from '../KeeperGradient';
 
@@ -15,13 +15,11 @@ const styles = StyleSheet.create({
   },
   textActive: {
     fontSize: 11,
-    color: '#2A6255',
     padding: 4,
   },
   text: {
     fontSize: 11,
     padding: 4,
-    color: '#FAFCFC'
   },
   containerBtn: {
     margin: 3,
@@ -30,7 +28,6 @@ const styles = StyleSheet.create({
   },
   containerBtnActive: {
     margin: 3,
-    backgroundColor: '#FAFCFC',
     borderRadius: 15,
     justifyContent: 'center',
     alignItems: 'center',
@@ -43,23 +40,31 @@ type Props = {
 };
 
 
+const containerBackgroundColorLight = ['#00836A', '#073E39']
+const containerBackgroundColorDark = ['#89AEA7', '#89AEA7']
 
 function Element(props) {
+  const { colorMode } = useColorMode();
+  const btnActiveBack = colorMode === 'dark' ? '#212726' : '#FAFCFC'
+  const btnBackTheme = colorMode === 'dark' ? containerBackgroundColorDark : containerBackgroundColorLight
+  const textColor = colorMode === 'dark' ? '#24312E' : '#FAFCFC'
+  const textActiveColor = colorMode === 'light' ? '#2A6255' : '#E3BE96'
   return (
-    <Box borderRadius={15} style={props.isActive ? styles.containerBtnActive : styles.containerBtn}>
-      <Text style={props.isActive ? styles.textActive : styles.text}>{props.title}</Text>
+    <Box borderRadius={15} style={props.isActive ? styles.containerBtnActive : styles.containerBtn} backgroundColor={props.isActive ? btnActiveBack : btnBackTheme}>
+      <Text style={props.isActive ? styles.textActive : styles.text} color={props.isActive ? textActiveColor : textColor}>{props.title}</Text>
     </Box>
   )
 }
 
 function MonthlyYearlySwitch({ value, onValueChange }: Props) {
+  const { colorMode } = useColorMode();
   return (
     <TouchableOpacity activeOpacity={0.6} onPress={onValueChange} style={styles.container} testID='btn_monthlyYearlySwitch'>
       <KeeperGradient
         start={[0, 0]}
         end={[1, 0]}
         style={styles.gradient}
-        colors={['#00836A', '#073E39']}
+        colors={colorMode === 'dark' ? containerBackgroundColorDark : containerBackgroundColorLight}
       >
         <Element isActive={value} title="Monthly" />
         <Element isActive={!value} title="Yearly" />
