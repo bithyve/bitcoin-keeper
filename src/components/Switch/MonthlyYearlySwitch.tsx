a929dc8452d0989e2375b87be970268e90b10718
akshay8551 <akshaychavan8551@gmail.com>
2023-08-02 13:56:16 +0530

    minor

--- src/components/Switch/MonthlyYearlySwitch.tsx
+++ src/components/Switch/MonthlyYearlySwitch.tsx
@@ -4,35 +4,37 @@ import { Box, useColorMode } from 'native-base';
 import Text from '../KeeperText';
 import KeeperGradient from '../KeeperGradient';
 
-const styles = StyleSheet.create({
-  container: {
-    alignSelf: 'flex-end',
-    marginRight: 10
-  },
-  gradient: {
-    borderRadius: 20,
-    flexDirection: 'row'
-  },
-  textActive: {
-    fontSize: 11,
-    padding: 4,
-  },
-  text: {
-    fontSize: 11,
-    padding: 4,
-  },
-  containerBtn: {
-    margin: 3,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  containerBtnActive: {
-    margin: 3,
-    borderRadius: 15,
-    justifyContent: 'center',
-    alignItems: 'center',
-  }
-})
+const getStyles = (btnActiveBack) =>
+  StyleSheet.create({
+    container: {
+      alignSelf: 'flex-end',
+      marginRight: 10
+    },
+    gradient: {
+      borderRadius: 20,
+      flexDirection: 'row'
+    },
+    textActive: {
+      fontSize: 11,
+      padding: 4,
+    },
+    text: {
+      fontSize: 11,
+      padding: 4,
+    },
+    containerBtn: {
+      margin: 3,
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    containerBtnActive: {
+      margin: 3,
+      borderRadius: 15,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: btnActiveBack
+    }
+  })
 
 type Props = {
   value: boolean;
@@ -46,11 +48,11 @@ const containerBackgroundColorDark = ['#89AEA7', '#89AEA7']
 function Element(props) {
   const { colorMode } = useColorMode();
   const btnActiveBack = colorMode === 'dark' ? '#212726' : '#FAFCFC'
-  const btnBackTheme = colorMode === 'dark' ? containerBackgroundColorDark : containerBackgroundColorLight
   const textColor = colorMode === 'dark' ? '#24312E' : '#FAFCFC'
   const textActiveColor = colorMode === 'light' ? '#2A6255' : '#E3BE96'
+  const styles = getStyles(btnActiveBack);
   return (
-    <Box borderRadius={15} style={props.isActive ? styles.containerBtnActive : styles.containerBtn} backgroundColor={props.isActive ? btnActiveBack : btnBackTheme}>
+    <Box borderRadius={15} style={props.isActive ? styles.containerBtnActive : styles.containerBtn}>
       <Text style={props.isActive ? styles.textActive : styles.text} color={props.isActive ? textActiveColor : textColor}>{props.title}</Text>
     </Box>
   )
@@ -58,6 +60,7 @@ function Element(props) {
 
 function MonthlyYearlySwitch({ value, onValueChange }: Props) {
   const { colorMode } = useColorMode();
+  const styles = getStyles('');
   return (
     <TouchableOpacity activeOpacity={0.6} onPress={onValueChange} style={styles.container} testID='btn_monthlyYearlySwitch'>
       <KeeperGradient
