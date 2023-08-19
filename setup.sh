beb8862fba4ca0c26292d929450b51dff54d3801
PraneethGunas <praneethgunas404@gmail.com>
2023-08-19 10:38:14 +0530

    added pod scripts back

--- setup.sh
+++ setup.sh
@@ -6,4 +6,4 @@ rn-nodeify --install buffer,events,process,stream,inherits,path,assert,crypto --
 cd ios && RCT_NEW_ARCH_ENABLED=0 pod install
 
 # android SDK location configuration
-cd android && touch local.properties && echo "sdk.dir = /Users/$(whoami)/Library/Android/sdk" >local.properties
+cd ../android && touch local.properties && echo "sdk.dir = /Users/$(whoami)/Library/Android/sdk" >local.properties
