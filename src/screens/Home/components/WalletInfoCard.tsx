97ed349be50ed9638b2dd8f36430fcf41b8e9a8d
akshay8551 <akshaychavan8551@gmail.com>
2024-02-26 10:04:35 +0530

    Home Screen - dark theme  #3955

--- src/screens/Home/components/WalletInfoCard.tsx
+++ src/screens/Home/components/WalletInfoCard.tsx
@@ -60,6 +60,7 @@ function WalletInfoCard({
             amount={amount}
             hideAmounts={!isShowAmount}
             fontSize={24}
+            color={colorMode === 'light' ? Colors.RichBlackDark : Colors.RichBlack}
             variation={colorMode === 'light' ? 'light' : 'dark'}
           />
         </TouchableOpacity>
