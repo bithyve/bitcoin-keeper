8eba82c65907a6ca833ae6a74866ed2b82cd21cc
Raheel1258 <rahilahmad1258@gmail.com>
2024-03-05 07:40:46 -0500

    adds todos

--- src/components/NotificationStack.tsx
+++ src/components/NotificationStack.tsx
@@ -168,6 +168,7 @@ function Card({ uai, index, totalLength, activeIndex }: CardProps) {
                         uaiSetActionFalse,
                         walletId: uai.entityId,
                         transferType: TransferType.WALLET_TO_VAULT,
+                        //--- TODO Pratyaksh ---
                         amount: 0,
                       })
                     : showToast('No vaults found', <ToastErrorIcon />);
