8d6ffb244c8ed9c20ef08022af93b3826fb80dc2
akshay8551 <akshaychavan8551@gmail.com>
2024-03-05 14:12:49 +0530

    removed unused variable

--- src/utils/PasswordTimeout.ts
+++ src/utils/PasswordTimeout.ts
@@ -1,13 +1,12 @@
 export const PasswordTimeout = (failedAttempts) => {
     const TIMEOUT = 60;
-    var waitingTime = 0;
     if (failedAttempts === 1) {
-        return waitingTime = TIMEOUT * 5
+        return TIMEOUT * 5
     } else if (failedAttempts === 2) {
-        return waitingTime = TIMEOUT * 15
+        return TIMEOUT * 15
     } else if (failedAttempts === 3) {
-        return waitingTime = TIMEOUT * 60
+        return TIMEOUT * 60
     } else {
-        return waitingTime = TIMEOUT * 5
+        return TIMEOUT * 5
     }
 }
