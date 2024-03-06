a4b07d56888239f8828b93d9e21445376f91142f
akshay8551 <akshaychavan8551@gmail.com>
2024-03-06 11:46:27 +0530

    Removed GreenishGrey color

--- src/theme/Colors.ts
+++ src/theme/Colors.ts
@@ -50,7 +50,7 @@ const Colors = {
   RussetBrown: 'rgba(145, 120, 93, 1)',
   RussetBrownLight: 'rgba(210, 194, 179, 1)',
   MintWhisper: 'rgba(45, 103, 89, 0.08)',
-  GreenishGrey: 'rgba(62, 82, 77, 1)',
+  // GreenishGrey: 'rgba(62, 82, 77, 1)',
   brownColor: 'rgba(145, 120, 92, 1)',
   learMoreTextcolor: 'rgba(242, 237, 230, 1)',
   Linen: 'rgba(240, 235, 230, 1)',
