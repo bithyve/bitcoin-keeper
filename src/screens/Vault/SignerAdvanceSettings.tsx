67deeddd1900c38e480be9ac5fd448715ebceb31
Parsh <parsh.cosmos11@gmail.com>
2024-03-19 14:56:43 +0530

    assisted keys: removed change signer type option

--- src/screens/Vault/SignerAdvanceSettings.tsx
+++ src/screens/Vault/SignerAdvanceSettings.tsx
@@ -376,7 +376,8 @@ function SignerAdvanceSettings({ route }: any) {
   const isInheritanceKey = signer.type === SignerType.INHERITANCEKEY;
   const isAppKey = signer.type === SignerType.KEEPER;
   const isMyAppKey = signer.type === SignerType.MY_KEEPER;
-  const isAssistedKey = isPolicyServer || isInheritanceKey || isAppKey || isMyAppKey;
+  const signersWithoutRegistration = isAppKey || isMyAppKey;
+  const isAssistedKey = isPolicyServer || isInheritanceKey;
 
   const isOtherSD = signer.type === SignerType.UNKOWN_SIGNER;
   const isTapsigner = signer.type === SignerType.TAPSIGNER;
@@ -420,7 +421,7 @@ function SignerAdvanceSettings({ route }: any) {
             }}
           />
         )}
-        {isAssistedKey || !vaultId ? null : (
+        {isAssistedKey || signersWithoutRegistration || !vaultId ? null : (
           <OptionCard
             title="Manual Registration"
             description="Register your active vault"
@@ -470,11 +471,13 @@ function SignerAdvanceSettings({ route }: any) {
             callback={navigateToScanPSBT}
           />
         )}
-        <OptionCard
-          title={isOtherSD ? 'Assign signer type' : 'Change signer type'}
-          description="Select from signer list"
-          callback={isOtherSD ? navigateToAssignSigner : () => setWarning(true)}
-        />
+        {isAssistedKey || signersWithoutRegistration ? null : (
+          <OptionCard
+            title={isOtherSD ? 'Assign signer type' : 'Change signer type'}
+            description="Select from signer list"
+            callback={isOtherSD ? navigateToAssignSigner : () => setWarning(true)}
+          />
+        )}
         <Box style={styles.signerText}>
           {`Signer used in ${signerVaults.length} wallet${signerVaults.length > 1 ? 's' : ''}`}
         </Box>
@@ -485,7 +488,7 @@ function SignerAdvanceSettings({ route }: any) {
               description={vault.presentationData?.description}
               cardName={vault.presentationData.name}
               icon={<WalletVault />}
-              callback={() => { }}
+              callback={() => {}}
             />
           ))}
         </ScrollView>
