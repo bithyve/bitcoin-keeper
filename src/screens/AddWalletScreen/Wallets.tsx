4be615ff917722de749ccdff8b016d08e5d1369e
Raheel1258 <rahilahmad1258@gmail.com>
2024-04-01 12:02:10 +0500

    resolves comment

--- src/screens/AddWalletScreen/Wallets.tsx
+++ src/screens/AddWalletScreen/Wallets.tsx
@@ -10,11 +10,12 @@ import { CommonActions } from '@react-navigation/native';
 import { VaultScheme } from 'src/services/wallets/interfaces/vault';
 import UpgradeSubscription from '../InheritanceToolsAndTips/components/UpgradeSubscription';
 import usePlan from 'src/hooks/usePlan';
+import { SubscriptionTier } from 'src/models/enums/SubscriptionTier';
 
 function Wallets({ navigation }) {
   const { wallets } = useWallets({ getAll: true });
   const { plan } = usePlan();
-  const isDiamondHand = plan === 'DIAMOND HANDS';
+  const isDiamondHand = plan === SubscriptionTier.L3.toUpperCase();
 
   const navigateToVaultSetup = (scheme: VaultScheme) => {
     navigation.dispatch(CommonActions.navigate({ name: 'VaultSetup', params: { scheme } }));
@@ -47,17 +48,17 @@ function Wallets({ navigation }) {
         callback={() => navigateToVaultSetup({ m: 2, n: 3 })}
       />
 
-      {isDiamondHand && <UpgradeSubscription type={'DIAMOND HANDS'} />}
+      {!isDiamondHand && <UpgradeSubscription type={'DIAMOND HANDS'} />}
 
       <OptionCard
-        disabled={isDiamondHand}
+        disabled={!isDiamondHand}
         title="3-of-5 Vault"
         description="Deep cold storage"
         LeftIcon={<VaultGreenIcon />}
         callback={() => navigateToVaultSetup({ m: 3, n: 5 })}
       />
       <OptionCard
-        disabled={isDiamondHand}
+        disabled={!isDiamondHand}
         title="Collaborative"
         description="With contacts/devices"
         LeftIcon={<CollaborativeIcon />}
