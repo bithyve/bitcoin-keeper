e27708e58208e6d0833c5877b8ede761bf0d59f1
PraneethGunas <praneethgunas404@gmail.com>
2024-04-08 17:00:57 +0530

    File export support for android

--- src/services/fs/index.ts
+++ src/services/fs/index.ts
@@ -2,30 +2,47 @@ import RNFS from 'react-native-fs';
 import Share from 'react-native-share';
 import { captureError } from 'src/services/sentry';
 import DocumentPicker from 'react-native-document-picker';
+import { Alert, PermissionsAndroid, Platform } from 'react-native';
 
+const saveToLocal = async (filePath, onError) => {
+  await Share.open({
+    url: 'file://' + filePath,
+    saveToFiles: true,
+    // @ts-ignore: Property 'useInternalStorage' exists in type 'ShareOptions' but is missing in type 'ShareOptions'.
+    useInternalStorage: Platform.OS === 'android',
+    failOnCancel: false,
+  })
+    .catch((error) => {
+      if (error.message === 'CANCELLED') {
+        return;
+      }
+      onError(error);
+    })
+    .finally(() => {
+      RNFS.unlink(filePath);
+    });
+};
 export const exportFile = async (fileData, fileName, onError) => {
   try {
     if (!fileData) {
       return;
     }
-    const filePath = RNFS.TemporaryDirectoryPath + fileName;
-    await RNFS.writeFile(filePath, fileData);
-    await Share.open({
-      url: 'file://' + filePath,
-      saveToFiles: true,
-      // @ts-ignore: Property 'useInternalStorage' exists in type 'ShareOptions' but is missing in type 'ShareOptions'.
-      useInternalStorage: Platform.OS === 'android',
-      failOnCancel: false,
-    })
-      .catch((error) => {
-        if (error.message === 'CANCELLED') {
-          return;
-        }
-        onError(error);
-      })
-      .finally(() => {
-        RNFS.unlink(filePath);
-      });
+    if (Platform.OS === 'ios') {
+      const filePath = RNFS.TemporaryDirectoryPath + fileName;
+      await RNFS.writeFile(filePath, fileData);
+      await saveToLocal(filePath, onError);
+    } else if (Platform.OS === 'android') {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED || Platform.Version >= 30) {
+        const filePath = RNFS.DownloadDirectoryPath + `/${fileName}`;
+        await RNFS.writeFile(filePath, fileData);
+        await saveToLocal(filePath, onError);
+      } else {
+        Alert.alert('Permission Denied!', 'You need to give storage permission to save the file');
+      }
+    }
   } catch (error) {
     captureError(error);
   }
