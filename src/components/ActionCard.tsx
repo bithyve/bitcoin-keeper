e0aa2de838cf1cb2d902844544bd7050288cea11
Abdul Rafay <abdulrafay141099@gmail.com>
2024-04-26 16:42:18 +0500

    Inheritance key for light mode added and Cards corrected

--- src/components/ActionCard.tsx
+++ src/components/ActionCard.tsx
@@ -34,8 +34,8 @@ function ActionCard({
         style={[
           styles.cardContainer,
           { ...customStyle },
-          { minHeight: isSmallDevice && hp(145) },
-          { minWidth: isSmallDevice && wp(110) },
+          // { minHeight: isSmallDevice && hp(145) },
+          // { minWidth: isSmallDevice && wp(110) },
         ]}
         backgroundColor={`${colorMode}.seashellWhite`}
       >
@@ -66,8 +66,8 @@ function ActionCard({
 
 const styles = StyleSheet.create({
   cardContainer: {
-    width: wp(104),
-    height: hp(114),
+    width: wp(115),
+    height: hp(155),
     paddingVertical: hp(10),
     paddingLeft: 10,
     paddingRight: 6,
