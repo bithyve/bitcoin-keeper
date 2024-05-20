1946ec98caeab54243d0ad6a4120d0d791d61593
Pratyaksh Chandra <pratyaksh.chandra2898@gmail.com>
2024-05-20 16:04:31 +0530

    config update cw-uai

--- src/components/NotificationStack.tsx
+++ src/components/NotificationStack.tsx
@@ -135,14 +135,14 @@ function Card({ uai, index, totalLength, activeIndex }: CardProps) {
   const [insightModal, setInsightModal] = useState(false);
   const { translations } = useContext(LocalizationContext);
   const { notification } = translations;
-  const skipUaiHandler = (uai: UAI) => {
-    dispatch(uaiActioned({ uaiId: uai.id, action: false }));
+  const skipUaiHandler = (uai: UAI, action = false) => {
+    dispatch(uaiActioned({ uaiId: uai.id, action }));
   };
 
-  const skipBtnConfig = (uai) => {
+  const skipBtnConfig = (uai: any, action?: boolean) => {
     return {
       text: 'Skip',
-      cta: () => skipUaiHandler(uai),
+      cta: () => skipUaiHandler(uai, action),
     };
   };
   const backupHistory = useQuery(RealmSchema.BackupHistory);
@@ -355,16 +355,16 @@ function Card({ uai, index, totalLength, activeIndex }: CardProps) {
         };
       case uaiType.CANARAY_WALLET:
         return {
-          heading: 'Canary Wallet Transaction',
-          body: uai.uaiDetails?.body || 'A transaction was detected in your canary wallet',
+          heading: 'Canary Wallet Accessed',
+          body: 'One of your key has been used',
           btnConfig: {
             primary: {
               text: 'View',
               cta: () => {
-                console.log('View the transaction');
+                navigtaion.navigate('VaultDetails', { vaultId: uai.entityId });
               },
             },
-            secondary: skipBtnConfig(uai),
+            secondary: skipBtnConfig(uai, true),
           },
         };
       default:
