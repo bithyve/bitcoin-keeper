2b83d630c8872bbeaf6aa890981861154d71e0b0
Raheel1258 <rahilahmad1258@gmail.com>
2024-05-21 19:22:28 +0500

    updates ui for import wallet

--- src/screens/Vault/VaultConfigurationRecreation.tsx
+++ src/screens/Vault/VaultConfigurationRecreation.tsx
@@ -1,66 +1,179 @@
-import { Keyboard, StyleSheet, TouchableWithoutFeedback, View } from 'react-native';
-import React, { useState } from 'react';
-import { Box, Input, useColorMode } from 'native-base';
-import { hp } from 'src/constants/responsive';
+import { Platform, StyleSheet, KeyboardAvoidingView } from 'react-native';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
+import { Box, Input, ScrollView, useColorMode } from 'native-base';
+import { hp, wp } from 'src/constants/responsive';
 import ScreenWrapper from 'src/components/ScreenWrapper';
 import KeeperHeader from 'src/components/KeeperHeader';
 import Buttons from 'src/components/Buttons';
 import useConfigRecovery from 'src/hooks/useConfigReocvery';
-import { useNavigation } from '@react-navigation/native';
-import { Tile } from '../NewKeeperAppScreen/NewKeeperAppScreen';
+import VaultGreenIcon from 'src/assets/images/vault_green.svg';
+
+import { RNCamera } from 'react-native-camera';
+import { URRegistryDecoder } from 'src/services/qr/bc-ur-registry';
+import { decodeURBytes } from 'src/services/qr';
+import { LocalizationContext } from 'src/context/Localization/LocContext';
+import OptionCard from 'src/components/OptionCard';
+import RNFS from 'react-native-fs';
+import DocumentPicker, { types } from 'react-native-document-picker';
+import Colors from 'src/theme/Colors';
+import UploadImage from 'src/components/UploadImage';
+import { ImageLibraryOptions, launchImageLibrary } from 'react-native-image-picker';
+import useToastMessage from 'src/hooks/useToastMessage';
+import { QRreader } from 'react-native-qr-decode-image-camera';
 
 function VaultConfigurationCreation() {
   const { colorMode } = useColorMode();
   const [inputText, setInputText] = useState('');
-  const { recoveryLoading, recoveryError, initateRecovery } = useConfigRecovery();
-  const { navigate } = useNavigation();
+  const { recoveryLoading, initateRecovery } = useConfigRecovery();
+
+  const [qrPercent, setQrPercent] = useState(0);
+  const [qrData, setData] = useState(0);
+  const { translations } = useContext(LocalizationContext);
+  const { showToast } = useToastMessage();
+  let decoder = new URRegistryDecoder();
+  const { common } = translations;
+
+  // eslint-disable-next-line no-promise-executor-return
+  const sleep = async (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+  const resetQR = async () => {
+    await sleep(3000);
+    setData(0);
+    setQrPercent(0);
+  };
+
+  useEffect(() => {
+    if (qrData) {
+      initateRecovery(qrData.toString());
+      resetQR();
+    }
+    return () => {
+      decoder = new URRegistryDecoder();
+    };
+  }, [qrData]);
+
+  const onBarCodeRead = (data) => {
+    if (!qrData) {
+      if (!data.data.startsWith('UR') && !data.data.startsWith('ur')) {
+        setData(data.data);
+        setQrPercent(100);
+      } else {
+        const { data: qrInfo, percentage } = decodeURBytes(decoder, data.data);
+        if (qrInfo) {
+          setData(qrInfo);
+        }
+        setQrPercent(percentage);
+      }
+    }
+  };
+
+  const handleDocumentSelection = useCallback(async () => {
+    try {
+      const response = await DocumentPicker.pick({
+        presentationStyle: 'fullScreen',
+        type: [types.docx, types.allFiles],
+        allowMultiSelection: false,
+      });
+      const content = await RNFS.readFile(response[0].uri, 'utf8');
+      initateRecovery(content);
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
+  const handleChooseImage = () => {
+    const options = {
+      quality: 1.0,
+      maxWidth: 500,
+      maxHeight: 500,
+      storageOptions: {
+        skipBackup: true,
+      },
+      mediaType: 'photo',
+    } as ImageLibraryOptions;
+
+    launchImageLibrary(options, async (response) => {
+      if (response.didCancel) {
+        showToast('Camera device has been canceled');
+      } else if (response.errorCode === 'camera_unavailable') {
+        showToast('Camera not available on device');
+      } else if (response.errorCode === 'permission') {
+        showToast('Permission not satisfied');
+      } else if (response.errorCode === 'others') {
+        showToast(response.errorMessage);
+      } else {
+        QRreader(response.assets[0].uri)
+          .then((data) => {
+            setData(data);
+          })
+          .catch((err) => {
+            showToast('Invalid or No related QR code');
+          });
+      }
+    });
+  };
 
   return (
     <ScreenWrapper barStyle="dark-content" backgroundcolor={`${colorMode}.primaryBackground`}>
-      <TouchableWithoutFeedback
-        onPress={() => {
-          Keyboard.dismiss();
-        }}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : null}
+        enabled
+        keyboardVerticalOffset={Platform.select({ ios: 8, android: 500 })}
+        style={styles.scrollViewWrapper}
       >
-        <View style={styles.wrapper}>
-          <KeeperHeader
-            title="Recover Using Wallet Configuration File"
-            subtitle="Recover the vault from output descriptor/configuration/BSMS File"
-          />
-          <Box style={styles.inputWrapper} backgroundColor={`${colorMode}.seashellWhite`}>
-            <Input
-              testID="input_walletConfigurationFile"
-              placeholder="Enter the Wallet Configuration File"
-              placeholderTextColor={`${colorMode}.primaryText`} // TODO: change to colorMode and use native base component
-              style={styles.textInput}
-              variant="unstyled"
-              value={inputText}
-              onChangeText={(text) => {
-                setInputText(text);
-              }}
-              textAlignVertical="top"
-              textAlign="left"
-              multiline
-            />
-          </Box>
-          <Box style={styles.tileContainer}>
-            <Box style={styles.tileWrapper}>
-              <Tile
-                title="Scan or Import a file"
-                subTitle="From your phone"
-                onPress={() => {
-                  navigate('ScanQRFileRecovery');
-                }}
+        <KeeperHeader title="Import a Wallet" subtitle="Using wallet configuration file" />
+        <ScrollView style={styles.scrollViewWrapper} showsVerticalScrollIndicator={false}>
+          <Box>
+            <Box style={styles.qrcontainer}>
+              <RNCamera
+                style={styles.cameraView}
+                captureAudio={false}
+                onBarCodeRead={onBarCodeRead}
+                useNativeZoom
               />
             </Box>
-            <Buttons
-              primaryCallback={() => initateRecovery(inputText)}
-              primaryText="Create Vault"
-              primaryLoading={recoveryLoading}
-            />
+            <Box style={styles.qrStatus}>
+              <UploadImage
+                backgroundColor={`${colorMode}.brownColor`}
+                onPress={handleChooseImage}
+              />
+            </Box>
+            <Box style={styles.optionsWrapper}>
+              <Box style={styles.inputWrapper} backgroundColor={`${colorMode}.seashellWhite`}>
+                <Input
+                  testID="input_walletConfigurationFile"
+                  placeholder="or enter configuration manually"
+                  placeholderTextColor={`${colorMode}.primaryText`} // TODO: change to colorMode and use native base component
+                  style={styles.textInput}
+                  variant="unstyled"
+                  value={inputText}
+                  onChangeText={(text) => {
+                    setInputText(text);
+                  }}
+                  textAlignVertical="top"
+                  textAlign="left"
+                  multiline
+                />
+              </Box>
+              <Box style={styles.separator} backgroundColor={`${colorMode}.lightSkin`}></Box>
+              <Box>
+                <OptionCard
+                  title="Upload a file"
+                  description="Select a file from your storage locations"
+                  LeftIcon={<VaultGreenIcon />}
+                  callback={handleDocumentSelection}
+                />
+              </Box>
+            </Box>
+            <Box style={styles.buttonContainer}>
+              <Buttons
+                primaryCallback={() => initateRecovery(inputText)}
+                primaryText={common.proceed}
+                primaryLoading={recoveryLoading}
+              />
+            </Box>
           </Box>
-        </View>
-      </TouchableWithoutFeedback>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </ScreenWrapper>
   );
 }
@@ -68,8 +181,8 @@ function VaultConfigurationCreation() {
 export default VaultConfigurationCreation;
 
 const styles = StyleSheet.create({
-  wrapper: {
-    flex: 1,
+  container: {
+    alignItems: 'center',
   },
   inputWrapper: {
     flexDirection: 'column',
@@ -81,18 +194,66 @@ const styles = StyleSheet.create({
   },
   textInput: {
     width: '100%',
+    alignSelf: 'center',
     borderTopLeftRadius: 10,
     borderBottomLeftRadius: 10,
     padding: 20,
     opacity: 0.5,
-    height: 150,
+    fontSize: 13,
+    height: hp(60),
   },
+
   tileContainer: {
     position: 'absolute',
-    bottom: 10,
+    bottom: -50,
     width: '100%',
   },
   tileWrapper: {
     marginBottom: 15,
+  },
+  qrcontainer: {
+    borderRadius: 10,
+    overflow: 'hidden',
+    marginVertical: 15,
+    alignItems: 'center',
+    height: hp(285),
+    width: wp(330),
+  },
+  cameraView: {
+    height: hp(285),
+    width: wp(330),
+  },
+  noteWrapper: {
+    width: '100%',
+    bottom: 0,
+    position: 'absolute',
+    paddingHorizontal: 20,
+  },
+  separator: {
+    width: '100%',
+    height: 2,
+    backgroundColor: Colors.WhiteCoffee,
+    marginBottom: 10,
+  },
+  optionsWrapper: {
+    marginHorizontal: wp(15),
+    alignContent: 'center',
+    alignItems: 'center',
+  },
+  buttonContainer: {
+    marginHorizontal: wp(15),
+
+    marginTop: hp(20),
+  },
+  qrStatus: {
+    position: 'absolute',
+    top: hp(255),
+    left: wp(90),
+    zIndex: 999,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  scrollViewWrapper: {
+    flex: 1,
   },
 });
