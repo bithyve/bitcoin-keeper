953ec50bdd6308a7569dfbf4e63f1e1847137a28
Raheel1258 <rahilahmad1258@gmail.com>
2024-05-23 14:34:45 +0500

    minor ui fixes

--- src/screens/Vault/AddSigningDevice.tsx
+++ src/screens/Vault/AddSigningDevice.tsx
@@ -804,8 +804,8 @@ function AddSigningDevice() {
         showButtons
         modalBackground={`${colorMode}.modalWhiteBackground`}
         textColor={`${colorMode}.primaryText`}
-        buttonTextColor={`${colorMode}.modalWhiteButtonText`}
-        buttonBackground={`${colorMode}.modalWhiteButton`}
+        buttonTextColor={`${colorMode}.buttonText`}
+        buttonBackground={`${colorMode}.greenButtonBackground`}
         subTitleColor={`${colorMode}.secondaryText`}
         subTitleWidth={wp(280)}
         showCloseIcon={false}
