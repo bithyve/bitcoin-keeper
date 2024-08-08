985ae5bb556d6781304550c1c9e2940711de310c
Parsh <parsh.cosmos11@gmail.com>
2024-08-08 12:09:03 +0530

    added: miniscript satisfier

--- src/services/wallets/operations/miniscript.ts
+++ src/services/wallets/operations/miniscript.ts
@@ -1,8 +1,10 @@
-import { compilePolicy, compileMiniscript } from '@bitcoinerlab/miniscript';
+import { compilePolicy, compileMiniscript, satisfier } from '@bitcoinerlab/miniscript';
+import { MultisigScriptType } from '../enums';
 
 export const generateMiniscript = (policy: string) => {
-  const { miniscript, issane } = compilePolicy(policy);
-  return { miniscript, issane };
+  const { miniscript, asm, issane } = compilePolicy(policy);
+  if (!issane) throw new Error('Miniscript is not sane');
+  return { miniscript, asm, issane };
 };
 
 /*
@@ -14,4 +16,10 @@ In other words, it should be a well-formed and standards-compliant script that c
 export const generateBitcoinScript = (miniscript: string) => {
   const { asm, issane } = compileMiniscript(miniscript);
   return { asm, issane };
+};
+
+export const generateScriptWitnesses = (policy: string) => {
+  const { miniscript } = compilePolicy(policy);
+  const { nonMalleableSats } = satisfier(miniscript);
+  return { scriptWitnesses: nonMalleableSats };
 };
