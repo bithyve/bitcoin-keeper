def108b094d2ac25bef8cb9b825ca7a4d40844e2
Raheel1258 <rahilahmad1258@gmail.com>
2024-09-19 20:46:56 +0500

    fixes passcode input lag on older devices

--- src/utils/utilities.ts
+++ src/utils/utilities.ts
@@ -212,3 +212,23 @@ export const trimCWDefaultName = (wallet) => {
   }
   return wallet;
 };
+
+export const throttle = (func, limit) => {
+  let lastFunc;
+  let lastRan;
+  return function (...args) {
+    const context = this;
+    if (!lastRan) {
+      func.apply(context, args);
+      lastRan = Date.now();
+    } else {
+      clearTimeout(lastFunc);
+      lastFunc = setTimeout(function () {
+        if (Date.now() - lastRan >= limit) {
+          func.apply(context, args);
+          lastRan = Date.now();
+        }
+      }, limit - (Date.now() - lastRan));
+    }
+  };
+};
