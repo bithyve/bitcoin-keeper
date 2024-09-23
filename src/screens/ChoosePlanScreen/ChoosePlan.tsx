225c6b41796bd3628582918037c03647fc2857d4
Raheel1258 <rahilahmad1258@gmail.com>
2024-09-23 11:41:46 +0500

    minor fix

--- src/screens/ChoosePlanScreen/ChoosePlan.tsx
+++ src/screens/ChoosePlanScreen/ChoosePlan.tsx
@@ -299,13 +299,10 @@ function ChoosePlan() {
         if (Platform.OS === 'android' && appSubscription.receipt) {
           purchaseTokenAndroid = JSON.parse(appSubscription.receipt).purchaseToken;
         }
-        processPurchase({
-          productId: sku,
-          transactionReceipt: 'keeper-dev-mock-purchase',
-          autoRenewingAndroid: true,
-          isCanceledAmazon: false,
-          transactionDate: Date.now(),
-          transactionId: `keeper-dev-mock-purchase-${Date.now()}`,
+        requestSubscription({
+          sku,
+          subscriptionOffers: [{ sku, offerToken }],
+          purchaseTokenAndroid,
         });
       }
     } catch (err) {
