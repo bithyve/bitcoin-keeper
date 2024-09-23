d35a2b938e552f2f6d5552a0883f986a476da2d0
Parsh <parsh.cosmos11@gmail.com>
2024-09-23 08:25:17 +0700

    generalize timelock encoding and introduces integer OP codes

--- src/services/wallets/operations/utils.ts
+++ src/services/wallets/operations/utils.ts
@@ -256,10 +256,23 @@ export default class WalletUtilities {
     }
 
     // prepare and enrich the time locks
-    const [T1, T2] = timelocks;
-    const encodedT1 = bitcoinJS.script.number.encode(T1).toString('hex');
-    const encodedT2 = bitcoinJS.script.number.encode(T2).toString('hex');
-    asm = asm.replace(`<${encodedT1}>`, encodedT1).replace(`<${encodedT2}>`, encodedT2);
+    for (const tl of timelocks) {
+      const encodedTL = bitcoinJS.script.number.encode(tl).toString('hex');
+      asm = asm.replace(`<${encodedTL}>`, encodedTL);
+    }
+
+    // Convert small integers to OP codes
+    asm = asm
+      .split(' ')
+      .map((token) => {
+        const num = parseInt(token);
+        if (Number.isNaN(num) === false && num >= 0 && num <= 16) {
+          return `OP_${num}`;
+        }
+        return token;
+      })
+      .join(' ');
+
     const script = bitcoinJS.script.fromASM(asm);
     return { script, subPaths, signerPubkeyMap };
   };
