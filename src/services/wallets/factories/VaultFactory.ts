469e56ed9d224db4f4138a535e75e3fe151df0a9
Parsh <parsh.cosmos11@gmail.com>
2024-09-24 08:12:06 +0700

    removed: redundant check

--- src/services/wallets/factories/VaultFactory.ts
+++ src/services/wallets/factories/VaultFactory.ts
@@ -114,10 +114,8 @@ export const generateVault = async ({
     shell: defaultShell,
   };
 
-  if (scheme.m > scheme.n) throw new Error(`scheme error: m:${scheme.m} > n:${scheme.n}`);
-
   const isMultiSig = scheme.n !== 1; // single xpub vaults are treated as single-sig wallet
-  const scriptType = isMultiSig ? ScriptTypes.P2WSH : ScriptTypes.P2WPKH; // TODO: find ways to accomodate P2TR 1-of-1 multisig(derivationConfig is not available on Vaults)
+  const scriptType = isMultiSig ? ScriptTypes.P2WSH : ScriptTypes.P2WPKH;
 
   const specs: VaultSpecs = {
     xpubs,
