80d5fba596d7d5a27a1612897c6ffbd92261e15d
benk10 <ben.kaufman10@gmail.com>
2024-10-23 11:10:42 +0400

    Return default nodes realm

--- src/storage/realm/schema/nodeConnect.ts
+++ src/storage/realm/schema/nodeConnect.ts
@@ -2,14 +2,22 @@ import { ObjectSchema } from 'realm';
 import { RealmSchema } from '../enum';
 
 // hosts own/private/user-provided nodes
+
+const nodeProperties = {
+  id: 'int',
+  host: 'string',
+  port: 'string',
+  useKeeperNode: 'bool',
+  isConnected: 'bool',
+  useSSL: 'bool',
+};
+
 export const NodeConnectSchema: ObjectSchema = {
   name: RealmSchema.NodeConnect,
-  properties: {
-    id: 'int',
-    host: 'string',
-    port: 'string',
-    useKeeperNode: 'bool',
-    isConnected: 'bool',
-    useSSL: 'bool',
-  },
+  properties: nodeProperties,
+};
+
+export const DefualtNodeConnectSchema: ObjectSchema = {
+  name: RealmSchema.DefaultNodeConnect,
+  properties: nodeProperties,
 };
