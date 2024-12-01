8a1bc65fb8c1e9a630659ddb8e7fc6847c2d1381
Raheel1258 <rahilahmad1258@gmail.com>
2024-12-02 01:29:44 +0500

    adds reset timeline option in signer settings

--- src/screens/Vault/SignerAdvanceSettings.tsx
+++ src/screens/Vault/SignerAdvanceSettings.tsx
@@ -1033,6 +1033,15 @@ function SignerAdvanceSettings({ route }: any) {
             callback={navigateToScanPSBT}
           />
         )}
+        <OptionCard
+          title={vaultTranslation.resetIKTitle}
+          description={vaultTranslation.resetIKDesc}
+          callback={() => {
+            navigation.dispatch(
+              CommonActions.navigate({ name: 'ResetInheritanceKey', params: { signerId } })
+            );
+          }}
+        />
         {isAssistedKey || signersWithoutRegistration ? null : (
           <OptionCard
             title={isOtherSD ? 'Assign signer type' : 'Change signer type'}
