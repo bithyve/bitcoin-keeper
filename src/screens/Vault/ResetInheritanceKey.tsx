f07e44b45b8dc7a4aae9921e49042392020da355
Parsh <parsh.cosmos11@gmail.com>
2024-12-04 16:27:46 +0100

    integrates: revaulting for MS-IK

--- src/screens/Vault/ResetInheritanceKey.tsx
+++ src/screens/Vault/ResetInheritanceKey.tsx
@@ -1,23 +1,31 @@
 import { Box, useColorMode } from 'native-base';
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext, useState, useEffect, useCallback } from 'react';
 import KeeperHeader from 'src/components/KeeperHeader';
 import ScreenWrapper from 'src/components/ScreenWrapper';
 import { Pressable, StyleSheet } from 'react-native';
 import { hp, wp } from 'src/constants/responsive';
 import Text from 'src/components/KeeperText';
-import { CommonActions, useNavigation } from '@react-navigation/native';
+import { CommonActions, useFocusEffect, useNavigation } from '@react-navigation/native';
 import Buttons from 'src/components/Buttons';
 import { LocalizationContext } from 'src/context/Localization/LocContext';
 import useSignerMap from 'src/hooks/useSignerMap';
-import { Signer } from 'src/services/wallets/interfaces/vault';
+import { Signer, Vault } from 'src/services/wallets/interfaces/vault';
 import moment from 'moment';
+import useToastMessage from 'src/hooks/useToastMessage';
+import ToastErrorIcon from 'src/assets/images/toast_error.svg';
+import { useDispatch } from 'react-redux';
+import WalletUtilities from 'src/services/wallets/operations/utils';
+import useVault from 'src/hooks/useVault';
+import { useAppSelector } from 'src/store/hooks';
+import { resetRealyVaultState } from 'src/store/reducers/bhr';
 import IKSInfocard from './components/IKSInfoCard';
 import { SDIcons } from './SigningDeviceIcons';
-import { MONTHS_12 } from './constants';
+import VaultMigrationController from './VaultMigrationController';
 
 function TimelineInfo({ duration, callback }) {
   const { colorMode } = useColorMode();
   const { translations } = useContext(LocalizationContext);
+
   const { vault: vaultText } = translations;
   return (
     <Box style={styles.timelineInfoContainer}>
@@ -37,24 +45,88 @@ function TimelineInfo({ duration, callback }) {
 }
 
 function ResetInheritanceKey({ route }) {
-  const { signerId }: { signerId: string } = route.params;
+  const { signerId, vault }: { signerId: string; vault: Vault } = route.params;
   const { colorMode } = useColorMode();
   const navigation = useNavigation();
   const { signerMap } = useSignerMap();
   const { translations } = useContext(LocalizationContext);
   const [selectedOption, setSelectedOption] = useState(null);
   const signer: Signer = signerMap[signerId];
+  const inheritanceSigner = vault.signers.find((signer) => signer.masterFingerprint === signerId);
+  const otherSigners = vault.signers.filter((signer) => signer.masterFingerprint !== signerId);
   const { vault: vaultText, common } = translations;
+  const { showToast } = useToastMessage();
+  const [generatedVaultId, setGeneratedVaultId] = useState('');
+  const { allVaults } = useVault({ includeArchived: false });
+  const newVault = allVaults.filter((v) => v.id === generatedVaultId)[0];
+  const [vaultCreating, setCreating] = useState(false);
+  const [currentBlockHeight, setCurrentBlockHeight] = useState(null);
+
+  const { relayVaultUpdate, relayVaultError, realyVaultErrorMessage } = useAppSelector(
+    (state) => state.bhr
+  );
+
+  const dispatch = useDispatch();
 
   const handleOptionChange = (option) => {
     setSelectedOption(option);
   };
+
+  const handleResetInheritanceKey = () => {
+    if (!selectedOption) {
+      showToast('Please select a timeline', <ToastErrorIcon />);
+    }
+    if (!currentBlockHeight) {
+      showToast('Failed to sync current block height', <ToastErrorIcon />);
+    }
+    setCreating(true);
+  };
+
+  useEffect(() => {
+    // should bind with a refresher in case the auto fetch for block-height fails
+    WalletUtilities.fetchCurrentBlockHeight()
+      .then(({ currentBlockHeight }) => {
+        setCurrentBlockHeight(currentBlockHeight);
+      })
+      .catch((err) => showToast(err));
+  }, []);
 
   useEffect(() => {
     if (route.params?.selectedOption) {
       setSelectedOption(route.params.selectedOption);
     }
   }, [route.params]);
+
+  useFocusEffect(
+    useCallback(() => {
+      if (relayVaultUpdate && newVault) {
+        dispatch(resetRealyVaultState());
+        setCreating(false);
+        // setVaultCreatedModalVisible(true);
+        const navigationState = {
+          index: 1,
+          routes: [
+            { name: 'Home' },
+            {
+              name: 'VaultDetails',
+              params: { vaultId: generatedVaultId, vaultTransferSuccessful: true },
+            },
+          ],
+        };
+        navigation.dispatch(CommonActions.reset(navigationState));
+      } else if (relayVaultUpdate) {
+        navigation.dispatch(CommonActions.reset({ index: 1, routes: [{ name: 'Home' }] }));
+        dispatch(resetRealyVaultState());
+        setCreating(false);
+      }
+
+      if (relayVaultError) {
+        showToast(realyVaultErrorMessage, <ToastErrorIcon />);
+        dispatch(resetRealyVaultState());
+        setCreating(false);
+      }
+    }, [relayVaultUpdate, relayVaultError, newVault, navigation, dispatch])
+  );
 
   return (
     <ScreenWrapper backgroundcolor={`${colorMode}.primaryBackground`}>
@@ -79,9 +151,27 @@ function ResetInheritanceKey({ route }) {
           />
         </Box>
         <Box>
-          <Buttons primaryText={vaultText.revaultNow} fullWidth />
+          <Buttons
+            primaryText={vaultText.revaultNow}
+            fullWidth
+            primaryCallback={handleResetInheritanceKey}
+          />
         </Box>
       </Box>
+      <VaultMigrationController
+        vaultCreating={vaultCreating}
+        vaultKeys={otherSigners}
+        scheme={{ m: vault.scheme.m, n: vault.scheme.n }}
+        name={vault.presentationData.name}
+        description={vault.presentationData.description}
+        vaultId={vault.id}
+        setGeneratedVaultId={setGeneratedVaultId}
+        vaultType={vault.type}
+        inheritanceKey={inheritanceSigner}
+        isAddInheritanceKey={true}
+        currentBlockHeight={currentBlockHeight}
+        selectedDuration={selectedOption?.label}
+      />
     </ScreenWrapper>
   );
 }
