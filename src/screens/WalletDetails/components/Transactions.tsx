ed2715d89cc4239123c02849ae2974172ffda541
Raheel1258 <rahilahmad1258@gmail.com>
2024-12-05 01:32:53 +0500

    consistent sortings for wallet and vault transactions

--- src/screens/WalletDetails/components/Transactions.tsx
+++ src/screens/WalletDetails/components/Transactions.tsx
@@ -50,10 +50,15 @@ function Transactions({ transactions, setPullRefresh, pullRefresh, currentWallet
     () =>
       [...transactions]
         .sort((a, b) => {
-          if (!a.blockTime && !b.blockTime) return 0;
-          if (!a.blockTime) return -1;
-          if (!b.blockTime) return 1;
-          return b.blockTime - a.blockTime;
+          // Sort unconfirmed transactions first
+          if (a.confirmations === 0 && b.confirmations !== 0) return -1;
+          if (a.confirmations !== 0 && b.confirmations === 0) return 1;
+
+          // Then sort by date
+          if (!a.date && !b.date) return 0;
+          if (!a.date) return -1;
+          if (!b.date) return 1;
+          return new Date(b.date).getTime() - new Date(a.date).getTime();
         })
         .slice(0, 5) || [],
     [transactions]
