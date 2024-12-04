c93f0c3643c29bc3301efb22f5fd021e54e332a4
Raheel1258 <rahilahmad1258@gmail.com>
2024-12-04 17:28:32 +0500

    shifts revault vault flow in to vault settings

--- src/screens/Vault/SignerAdvanceSettings.tsx
+++ src/screens/Vault/SignerAdvanceSettings.tsx
@@ -1034,17 +1034,6 @@ function SignerAdvanceSettings({ route }: any) {
             callback={navigateToScanPSBT}
           />
         )}
-        {vaultId && (
-          <OptionCard
-            title={vaultTranslation.resetIKTitle}
-            description={vaultTranslation.resetIKDesc}
-            callback={() => {
-              navigation.dispatch(
-                CommonActions.navigate({ name: 'ResetInheritanceKey', params: { signerId } })
-              );
-            }}
-          />
-        )}
         {isAssistedKey || signersWithoutRegistration ? null : (
           <OptionCard
             title={isOtherSD ? 'Assign signer type' : 'Change signer type'}
