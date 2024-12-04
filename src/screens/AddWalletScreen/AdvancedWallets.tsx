29c7a13daeab1d98337f6351b0b3c5adac14e73f
Raheel1258 <rahilahmad1258@gmail.com>
2024-12-04 12:58:49 +0500

    disables time-lock and assisted wallet option

--- src/screens/AddWalletScreen/AdvancedWallets.tsx
+++ src/screens/AddWalletScreen/AdvancedWallets.tsx
@@ -3,6 +3,8 @@ import React, { useContext } from 'react';
 import OptionCard from 'src/components/OptionCard';
 import TimeLockIcon from 'src/assets/images/calendar.svg';
 import AssistedVaultIcon from 'src/assets/images/assisted-vault-icon.svg';
+import TimeLockDisabledIcon from 'src/assets/images/calendar-disabled.svg';
+import AssistedVaultDisabledIcon from 'src/assets/images/assisted-vault-disabled-icon.svg';
 import VaultGreenIcon from 'src/assets/images/vault_green.svg';
 import { CommonActions } from '@react-navigation/native';
 import CardPill from 'src/components/CardPill';
@@ -13,6 +15,7 @@ function AdvancedWallets({ navigation }) {
   const { colorMode } = useColorMode();
   const { translations } = useContext(LocalizationContext);
   const { importWallet } = translations;
+  const isDisbaled = true;
   const navigateToVaultSetup = (scheme?, isTimeLock = false) => {
     navigation.dispatch(
       CommonActions.navigate({ name: 'VaultSetup', params: { scheme, isTimeLock } })
@@ -22,22 +25,38 @@ function AdvancedWallets({ navigation }) {
   return (
     <Box>
       <OptionCard
+        disabled={isDisbaled}
         title="Time Lock"
         description="For 3, 6 or 12 months"
-        LeftIcon={<TimeLockIcon />}
+        LeftIcon={isDisbaled ? <TimeLockDisabledIcon /> : <TimeLockIcon />}
         titleColor={`${colorMode}.primaryText`}
         descriptionColor={`${colorMode}.secondaryText`}
         callback={() => navigateToVaultSetup({ m: 2, n: 3 }, true)}
+        CardPill={
+          <CardPill
+            heading="COMING SOON"
+            backgroundColor={`${colorMode}.greenPillBackground`}
+            headingColor={`${colorMode}.white`}
+          />
+        }
       />
       <OptionCard
+        disabled={isDisbaled}
         title="Assisted Wallet"
-        description="Create degrading Multisig with advisors"
-        LeftIcon={<AssistedVaultIcon />}
+        description={`Create degrading mulit-key\nwallet with friends or advisors`}
+        LeftIcon={isDisbaled ? <AssistedVaultDisabledIcon /> : <AssistedVaultIcon />}
         titleColor={`${colorMode}.primaryText`}
         descriptionColor={`${colorMode}.secondaryText`}
         callback={() => {
           navigation.navigate('AssistedWalletTimeline');
         }}
+        CardPill={
+          <CardPill
+            heading="COMING SOON"
+            backgroundColor={`${colorMode}.greenPillBackground`}
+            headingColor={`${colorMode}.white`}
+          />
+        }
       />
       <OptionCard
         title="Custom Multisig"
