020e12598fd27170164bbe00b29d1cc848dd97d8
Raheel1258 <rahilahmad1258@gmail.com>
2024-12-05 13:15:07 +0500

    removes comment

--- src/screens/Vault/VaultDetails.tsx
+++ src/screens/Vault/VaultDetails.tsx
@@ -321,8 +321,6 @@ function VaultDetails({ navigation, route }: ScreenProps) {
 
   useEffect(() => {
     if (!syncing && syncingCompleted && transactionToast) {
-      console;
-      console.log('Transaction Toast');
       showToast(
         vaultTranslation.transactionToastMessage,
         <TickIcon />,
