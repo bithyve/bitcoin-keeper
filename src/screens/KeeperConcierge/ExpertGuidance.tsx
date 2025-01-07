32503e1b93462a43c588ac045697198ff37c9499
cakesoft-vaibhav <vaibhav@cakesofttech.com>
2025-01-07 16:10:05 +0530

    feat: ExpertFilter

--- src/screens/KeeperConcierge/ExpertGuidance.tsx
+++ src/screens/KeeperConcierge/ExpertGuidance.tsx
@@ -1,11 +1,11 @@
 import { Box, ScrollView, useColorMode } from 'native-base';
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext, useEffect, useRef, useMemo } from 'react';
 import { ActivityIndicator, StyleSheet } from 'react-native';
 import ConciergeHeader from './components/ConciergeHeader';
 import ConciergeScreenWrapper from './components/ConciergeScreenWrapper';
 import ContentWrapper from '../../components/ContentWrapper';
 import { hp, wp } from 'src/constants/responsive';
-import ExpertCard from './components/ExpertCard';
+import ExpertCard, { ExpertExpertise } from './components/ExpertCard';
 import KeeperTextInput from 'src/components/KeeperTextInput';
 import Text from 'src/components/KeeperText';
 import DisabledOverlay from 'src/components/DisabledOverlay';
@@ -14,15 +14,21 @@ import usePlan from 'src/hooks/usePlan';
 import DisabledExpertGuidance from './components/DisabledExpertGuidance';
 import { LocalizationContext } from 'src/context/Localization/LocContext';
 import Relay from 'src/services/backend/Relay';
+import useToastMessage from 'src/hooks/useToastMessage';
+import ToastErrorIcon from 'src/assets/images/toast_error.svg';
+import { useNavigation } from '@react-navigation/native';
 
 const ExpertGuidance = () => {
   const { colorMode } = useColorMode();
+  const navigation = useNavigation();
   const { plan } = usePlan();
   const { translations } = useContext(LocalizationContext);
   const { concierge } = translations;
   const [search, setSearch] = useState('');
   const isPleb = plan === SubscriptionTier.L1.toUpperCase();
   const [advisors, setAdvisors] = useState(null);
+  const allAdvisors = useRef(null);
+  const { showToast } = useToastMessage();
 
   useEffect(() => {
     loadAdvisors();
@@ -31,17 +37,52 @@ const ExpertGuidance = () => {
   const loadAdvisors = async () => {
     try {
       const response = await Relay.fetchAdvisorsList();
+      allAdvisors.current = response;
       setAdvisors(response);
     } catch (error) {
-      console.log('Error loading advisors', error);
+      showToast('Something went wrong. Please try again later.', <ToastErrorIcon />);
+      navigation.goBack();
     }
   };
+
+  const filteredAdvisors = useMemo(() => {
+    if (!search.length) return allAdvisors?.current;
+    return allAdvisors?.current?.filter((advisor) => {
+      const lowerSearch = search.toLowerCase();
+
+      const matchesName = advisor.name.toLowerCase().includes(lowerSearch);
+      const matchesExpertise = advisor.expertise.some((exp: ExpertExpertise) =>
+        exp.expertise.toLowerCase().includes(lowerSearch)
+      );
+      const matchesLanguage = advisor.details.language.some((lang: string) =>
+        lang.toLowerCase().includes(lowerSearch)
+      );
+      const matchesTimeZone = advisor.details.timezone.toLowerCase().includes(lowerSearch);
+      const matchesExperience = advisor.details.experience.toLowerCase().includes(lowerSearch);
+      const matchesLocation = advisor.location.toLowerCase().includes(lowerSearch);
+
+      return (
+        matchesName ||
+        matchesExpertise ||
+        matchesLanguage ||
+        matchesTimeZone ||
+        matchesExperience ||
+        matchesLocation
+      );
+    });
+  }, [search]);
+
+  useEffect(() => {
+    if (allAdvisors.current) {
+      setAdvisors(filteredAdvisors);
+    }
+  }, [search]);
 
   return (
     <ConciergeScreenWrapper backgroundcolor={`${colorMode}.pantoneGreen`} barStyle="light-content">
       <ConciergeHeader title={concierge.expertGuidanceTitle} />
       <ContentWrapper backgroundColor={`${colorMode}.primaryBackground`}>
-        <DisabledOverlay visible={isPleb} bottomComponent={<DisabledExpertGuidance />} />
+        <DisabledOverlay visible={false} bottomComponent={<DisabledExpertGuidance />} />
         <Box style={styles.searchBox}>
           <KeeperTextInput
             placeholder={` ${concierge.searchForAnExpert}`}
