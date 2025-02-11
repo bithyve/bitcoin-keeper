19ee493983c23202d6209a4150465d23168a91a6
benk10 <ben.kaufman10@gmail.com>
2025-02-11 10:18:57 -0500

    Fix specter wallet registration

--- src/screens/QRScreens/RegisterWithQR.tsx
+++ src/screens/QRScreens/RegisterWithQR.tsx
@@ -35,18 +35,13 @@ function RegisterWithQR({ route, navigation }: any) {
   const { signer } = useSignerFromKey(vaultKey);
   const walletConfig =
     signer.type === SignerType.SPECTER
-      ? activeVault.scheme.miniscriptScheme
-        ? `addwallet ${activeVault.presentationData.name}&${generateOutputDescriptors(
-            activeVault,
-            false,
-            false
-          )
-            .replace('/**', '/{0,1}/*')
-            .replace(/<(\d+);(\d+)>/g, '{$1,$2}')}`
-        : `${SPECTER_PREFIX}${generateOutputDescriptors(activeVault, false, false).replaceAll(
-            '/<0;1>/*',
-            ''
-          )}`
+      ? `addwallet ${activeVault.presentationData.name}&${generateOutputDescriptors(
+          activeVault,
+          false,
+          false
+        )
+          .replace('/**', '/{0,1}/*')
+          .replace(/<(\d+);(\d+)>/g, '{$1,$2}')}`
       : activeVault.scheme.miniscriptScheme
       ? generateOutputDescriptors(activeVault)
       : getWalletConfig({ vault: activeVault, signerType: signer.type });
