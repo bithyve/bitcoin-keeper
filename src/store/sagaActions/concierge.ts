cc18861b04ff6eac695c15335bfb35e2689a64f5
cakesoft-vaibhav <vaibhav@cakesofttech.com>
2025-02-19 18:00:16 +0530

    fix: remove ZendeskMessaging integration

--- src/store/sagaActions/concierge.ts
+++ src/store/sagaActions/concierge.ts
@@ -1,25 +1,9 @@
 import { ConciergeTag } from 'src/models/enums/ConciergeTag';
 
-export const GO_TO_CONCEIERGE = 'GO_TO_CONCEIERGE';
-export const OPEN_CONCEIERGE = 'OPEN_CONCEIERGE';
 export const LOAD_CONCIERGE_USER = 'LOAD_CONCIERGE_USER';
 export const ADD_TICKET_STATUS_UAI = 'ADD_TICKET_STATUS_UAI';
 export const SCHEDULE_ONBOARDING_CALL = 'SCHEDULE_ONBOARDING_CALL';
 
-export const goToConcierge = (tags: ConciergeTag[], screenName = '') => ({
-  type: GO_TO_CONCEIERGE,
-  payload: {
-    tags,
-    screenName,
-  },
-});
-
-export const openConcierge = (dontShow: boolean) => ({
-  type: OPEN_CONCEIERGE,
-  payload: {
-    dontShow,
-  },
-});
 
 export const loadConciergeUser = () => ({
   type: LOAD_CONCIERGE_USER,
