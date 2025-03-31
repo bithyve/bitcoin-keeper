2f8327a2cfa6603a357829674c607a6db8704aec
cakesoft-vaibhav <vaibhav@cakesofttech.com>
2025-03-31 13:48:18 +0530

    feat:populateNodes

--- src/store/sagas/settings.ts
+++ src/store/sagas/settings.ts
@@ -1,9 +1,18 @@
-import { call, put } from 'redux-saga/effects';
+import { call, put, select } from 'redux-saga/effects';
 import { createWatcher } from '../utilities';
 import { setBitcoinNetwork } from '../reducers/settings';
 import { CHANGE_BITCOIN_NETWORK } from '../sagaActions/settings';
 import Node from 'src/services/electrum/node';
 import { fetchFeeRates } from '../sagaActions/send_and_receive';
+import { NetworkType } from 'src/services/wallets/enums';
+import {
+  predefinedMainnetNodes,
+  predefinedTestnetNodes,
+} from 'src/services/electrum/predefinedNodes';
+import dbManager from 'src/storage/realm/dbManager';
+import { RealmSchema } from 'src/storage/realm/enum';
+import { RootState } from '../store';
+import ElectrumClient from 'src/services/electrum/client';
 
 function* changeBitcoinNetworkWorker({ payload }) {
   let activeNode;
@@ -18,6 +27,32 @@ function* changeBitcoinNetworkWorker({ payload }) {
     if (activeNode) {
       yield call(Node.disconnect, activeNode);
     }
+    const bitcoinNetworkType = yield select(
+      (state: RootState) => state.settings.bitcoinNetworkType
+    );
+
+    // Add default nodes if not exists and connect
+    if (
+      !Node.getAllNodes().filter((node) =>
+        bitcoinNetworkType === NetworkType.TESTNET
+          ? node.host.includes('testnet')
+          : !node.host.includes('testnet')
+      ).length
+    )
+      yield call(
+        dbManager.createObjectBulk,
+        RealmSchema.NodeConnect,
+        network === NetworkType.TESTNET ? predefinedTestnetNodes : predefinedMainnetNodes
+      );
+
+    ElectrumClient.setActivePeer(
+      Node.getAllNodes().filter((node) =>
+        bitcoinNetworkType === NetworkType.TESTNET
+          ? node.host.includes('testnet')
+          : !node.host.includes('testnet')
+      )
+    );
+    yield call(ElectrumClient.connect);
   } catch (error) {
     console.log('🚀 ~ function*changeBitcoinNetworkWorker ~ error:', error);
   }
