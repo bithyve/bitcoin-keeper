76270bc5eac98d16741e50e3c0c839ab50992856
cakesoft-vaibhav <vaibhav@cakesofttech.com>
2025-04-01 12:19:03 +0530

    fix: realmMigrationForRemovedTxNodeFromVault/Wallet

--- src/storage/realm/realm.ts
+++ src/storage/realm/realm.ts
@@ -9,7 +9,7 @@ export class RealmDatabase {
 
   public static file = 'keeper.realm';
 
-  public static schemaVersion = 92;
+  public static schemaVersion = 93;
 
   /**
    * initializes/opens realm w/ appropriate configuration
