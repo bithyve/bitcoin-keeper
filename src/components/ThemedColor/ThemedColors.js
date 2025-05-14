aa2b7dd1620bf578991728b9909da96d12c0c987
Raheel1258 <rahilahmad1258@gmail.com>
2025-05-14 15:39:06 +0500

    setup

---
+++ src/components/ThemedColor/ThemedColors.js
@@ -0,0 +1,12 @@
+import Colors from 'src/theme/Colors';
+
+const themeColors = {
+  footer_selected_option: {
+    DARK: Colors.headerWhite,
+    LIGHT: Colors.primaryGreen,
+    PRIVATE: Colors.goldenGradient,
+    PRIVATE_LIGHT: Colors.goldenGradient,
+  },
+};
+
+export default themeColors;
