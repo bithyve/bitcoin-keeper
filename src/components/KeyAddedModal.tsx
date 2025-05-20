544f46d85c31d6190c7d56e0e35d9f84215cc88e
Raheel1258 <rahilahmad1258@gmail.com>
2025-05-20 10:58:28 +0500

    signer addded modal

--- src/components/KeyAddedModal.tsx
+++ src/components/KeyAddedModal.tsx
@@ -6,10 +6,139 @@ import { hp, wp } from 'src/constants/responsive';
 import { StyleSheet } from 'react-native';
 import { getAccountFromSigner } from 'src/utils/utilities';
 import ThemedSvg from './ThemedSvg.tsx/ThemedSvg';
+import { SignerType } from 'src/services/wallets/enums';
 
 function KeyAddedModal({ visible, close, signer }) {
   const navigation = useNavigation();
   const { colorMode } = useColorMode();
+
+  const KeyAddedIllustration = ({ signer }) => {
+    let Illustration;
+
+    switch (signer.type) {
+      case SignerType.COLDCARD:
+        Illustration = (
+          <ThemedSvg
+            name={'coldCard_success_illustration'}
+            width={wp(200)}
+            height={hp(200)}
+            style={styles.externalKeyIllustration}
+          />
+        );
+        break;
+      case SignerType.BITBOX02:
+        Illustration = (
+          <ThemedSvg
+            name={'bitBox_success'}
+            width={wp(200)}
+            height={hp(200)}
+            style={styles.externalKeyIllustration}
+          />
+        );
+        break;
+      case SignerType.JADE:
+        Illustration = (
+          <ThemedSvg
+            name={'jade_success'}
+            width={wp(200)}
+            height={hp(200)}
+            style={styles.externalKeyIllustration}
+          />
+        );
+        break;
+      case SignerType.KEYSTONE:
+        Illustration = (
+          <ThemedSvg
+            name={'keyStone_success'}
+            width={wp(200)}
+            height={hp(200)}
+            style={styles.externalKeyIllustration}
+          />
+        );
+        break;
+      case SignerType.LEDGER:
+        Illustration = (
+          <ThemedSvg
+            name={'ledger_success'}
+            width={wp(200)}
+            height={hp(200)}
+            style={styles.externalKeyIllustration}
+          />
+        );
+        break;
+      case SignerType.PASSPORT:
+        Illustration = (
+          <ThemedSvg
+            name={'foundation_success'}
+            width={wp(200)}
+            height={hp(200)}
+            style={styles.externalKeyIllustration}
+          />
+        );
+        break;
+      case SignerType.PORTAL:
+        Illustration = (
+          <ThemedSvg
+            name={'portal_success'}
+            width={wp(200)}
+            height={hp(200)}
+            style={styles.externalKeyIllustration}
+          />
+        );
+        break;
+      case SignerType.SEEDSIGNER:
+        Illustration = (
+          <ThemedSvg
+            name={'seedSigner_success'}
+            width={wp(200)}
+            height={hp(200)}
+            style={styles.externalKeyIllustration}
+          />
+        );
+        break;
+      case SignerType.SPECTER:
+        Illustration = (
+          <ThemedSvg
+            name={'specter_success'}
+            width={wp(200)}
+            height={hp(200)}
+            style={styles.externalKeyIllustration}
+          />
+        );
+        break;
+      case SignerType.TAPSIGNER:
+        Illustration = (
+          <ThemedSvg
+            name={'tapSigner_success'}
+            width={wp(200)}
+            height={hp(200)}
+            style={styles.externalKeyIllustration}
+          />
+        );
+        break;
+      case SignerType.TREZOR:
+        Illustration = (
+          <ThemedSvg
+            name={'trezor_success'}
+            width={wp(200)}
+            height={hp(200)}
+            style={styles.externalKeyIllustration}
+          />
+        );
+        break;
+      default:
+        Illustration = (
+          <ThemedSvg
+            name={'success_illustration'}
+            width={wp(200)}
+            height={hp(200)}
+            style={styles.externalKeyIllustration}
+          />
+        );
+    }
+
+    return Illustration;
+  };
 
   const defaultConfig = {
     buttonText: 'Add Description',
@@ -30,11 +159,27 @@ function KeyAddedModal({ visible, close, signer }) {
 
   const { buttonText, buttonCallback, secondaryButtonText, content } = defaultConfig;
 
+  const getSignerTitle = (signerType) => {
+    if (signerType === SignerType.MY_KEEPER) {
+      return 'Mobile Key Added Successfully!';
+    } else if (signerType === SignerType.KEEPER) {
+      return 'External Key Added Successfully!';
+    } else if (signerType === SignerType.POLICY_SERVER) {
+      return 'Server Key Added Successfully!';
+    } else if (signerType === SignerType.OTHER_SD) {
+      return 'Other Signer Added Successfully!';
+    } else if (signerType === SignerType.SEED_WORDS) {
+      return 'Seed key Added Successfully!';
+    }
+    const formattedType = signerType.charAt(0).toUpperCase() + signerType.slice(1).toLowerCase();
+    return `${formattedType} Added Successfully!`;
+  };
+
   return (
     signer && (
       <KeeperModal
         visible={visible}
-        title="Key Added Successfully!"
+        title={getSignerTitle(signer.type)}
         subTitle={`${
           getAccountFromSigner(signer) !== 0
             ? `Account #${getAccountFromSigner(signer)} of the key was successfully added. `
@@ -51,13 +196,7 @@ function KeyAddedModal({ visible, close, signer }) {
         secondaryCallback={close}
         Content={() => (
           <Box style={styles.externalKeyModal}>
-            <ThemedSvg
-              name={'success_illustration'}
-              width={wp(200)}
-              height={hp(200)}
-              style={styles.externalKeyIllustration}
-            />
-
+            <KeyAddedIllustration signer={signer} />
             {content}
           </Box>
         )}
