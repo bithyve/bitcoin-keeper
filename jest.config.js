c938350e7a901974922027c62aa2bedf943c18b7
Parsh <parsh.cosmos11@gmail.com>
2025-05-30 16:17:08 +0200

    restored: module-name mapper

--- jest.config.js
+++ jest.config.js
@@ -8,14 +8,14 @@ module.exports = {
   ],
 
   moduleNameMapper: {
-    '^realm$': '<rootDir>/__mocks__/realm.js',
-    '\\.svg$': '<rootDir>/__mocks__/svgMock.js',
-    '^react-native-fs$': '<rootDir>/__mocks__/react-native-fs.js',
-    '^react-native-nfc-manager$': '<rootDir>/__mocks__/react-native-nfc-manager.js',
-    '^redux-persist/lib/storage$': '<rootDir>/__mocks__/redux-persist/lib/storage.js',
-    '^@react-native-clipboard/clipboard$': '<rootDir>/__mocks__/react-native-clipboard.js',
-    '^@react-native-community/netinfo$': '<rootDir>/__mocks__/react-native-community-netinfo.js',
-    '^@react-native-firebase/messaging$': '<rootDir>/__mocks__/react-native-firebase-messaging.js',
+    // '^realm$': '<rootDir>/__mocks__/realm.js',
+    // '\\.svg$': '<rootDir>/__mocks__/svgMock.js',
+    // '^react-native-fs$': '<rootDir>/__mocks__/react-native-fs.js',
+    // '^react-native-nfc-manager$': '<rootDir>/__mocks__/react-native-nfc-manager.js',
+    // '^redux-persist/lib/storage$': '<rootDir>/__mocks__/redux-persist/lib/storage.js',
+    // '^@react-native-clipboard/clipboard$': '<rootDir>/__mocks__/react-native-clipboard.js',
+    // '^@react-native-community/netinfo$': '<rootDir>/__mocks__/react-native-community-netinfo.js',
+    // '^@react-native-firebase/messaging$': '<rootDir>/__mocks__/react-native-firebase-messaging.js',
   },
   moduleDirectories: ['node_modules', 'src'],
 };
