3dd58b4818803af33636db0fea23849b112543ef
Parsh <parsh.cosmos11@gmail.com>
2025-06-11 13:07:28 +0200

    adds: coverage stats

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,10 @@ module.exports = {
   transformIgnorePatterns: [
     'node_modules/(?!(jest-)?@?react-native|@react-navigation|@react-native-community|redux-persist|@realm)',
   ],
-
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['src/services/electrum/**/*.ts', 'src/services/wallets/**/*.ts'],
+  coverageReporters: ['text', 'lcov'],
   moduleNameMapper: {
     '^@react-native-clipboard/clipboard$': '<rootDir>/__mocks__/react-native-clipboard.js',
     '^@react-native-community/netinfo$': '<rootDir>/__mocks__/react-native-community-netinfo.js',
