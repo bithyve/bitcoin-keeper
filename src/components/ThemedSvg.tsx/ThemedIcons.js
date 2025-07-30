208a1a66783ce1d19f3f9ef530954ea76eb92239
Raheel1258 <rahilahmad1258@gmail.com>
2025-07-30 16:30:47 +0500

    fix logo

--- src/components/ThemedSvg.tsx/ThemedIcons.js
+++ src/components/ThemedSvg.tsx/ThemedIcons.js
@@ -337,6 +337,7 @@ import SwitchLightLogo from 'src/assets/images/switch-light-logo.svg';
 import SwitchDarkLogo from 'src/assets/images/switch-dark-logo.svg';
 import SwapDownGreenIcon from 'src/assets/images/swap-down-green-icon.svg';
 import SwapDownWhiteIcon from 'src/assets/images/swap-down-white-icon.svg';
+import BuyBtcPRivate from 'src/assets/privateImages/buy-btc-header-private.svg';
 
 const themeIcons = {
   edit_profile_icons: {
@@ -1149,7 +1150,7 @@ const themeIcons = {
   header_buy_btc: {
     DARK: BtcLogo,
     LIGHT: BtcLogo,
-    PRIVATE: AquireHeadrWhite,
+    PRIVATE: BuyBtcPRivate,
     PRIVATE_LIGHT: BtcLogoGold,
   },
   footer_buy_btc: {
