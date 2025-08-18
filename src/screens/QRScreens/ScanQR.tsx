b38f2e4de0c8b89177faee906ac6d6a18e7cffab
cakesoft-vaibhav <vaibhav@cakesofttech.com>
2025-08-18 14:24:58 +0530

    fix: AddContactKBOverlap

--- src/screens/QRScreens/ScanQR.tsx
+++ src/screens/QRScreens/ScanQR.tsx
@@ -27,6 +27,7 @@ import Instruction from 'src/components/Instruction';
 import ThemedSvg from 'src/components/ThemedSvg.tsx/ThemedSvg';
 import Buttons from 'src/components/Buttons';
 import ShareDark from 'src/assets/images/share-white.svg';
+import { useKeyboard } from 'src/hooks/useKeyboard';
 
 const decoder = new URRegistryDecoder();
 
@@ -64,6 +65,7 @@ function ScanQR() {
   const isDarkMode = colorMode === 'dark';
   const isHealthCheck = mode === InteracationMode.HEALTH_CHECK;
   const [infoModal, setInfoModal] = useState(false);
+  const isKeyboardOpen = useKeyboard();
 
   const onTextSubmit = (data) => {
     if (!data.startsWith('UR') && !data.startsWith('ur')) {
@@ -166,7 +168,7 @@ function ScanQR() {
               <Note title={common.note} subtitle={common.scanQRNote} />
             </Box>
           )}
-          {contactShareData && (
+          {!isKeyboardOpen && contactShareData && (
             <Box style={styles.noteWrapper}>
               <Buttons
                 primaryText={contactText.shareContact}
