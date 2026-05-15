27dfe53abbd29b738c1487cfa1ea50d3e3d1a86c
cakesoft-vaibhav <vaibhav@cakesofttech.com>
2026-05-15 11:32:04 +0530

    fix: Empty Wallet and Signer view illustration removed since it was causing ui issue on ios | fixed app crash on Collaborative Wallet flow due to HCE context on ios

--- src/screens/AppSettings/ManageWallets.tsx
+++ src/screens/AppSettings/ManageWallets.tsx
@@ -10,7 +10,6 @@ import { EntityKind, VaultType, VisibilityType } from 'src/services/wallets/enum
 import { Wallet } from 'src/services/wallets/interfaces/wallet';
 import CollaborativeIcon from 'src/assets/images/collaborative_vault_white.svg';
 import WalletIcon from 'src/assets/images/daily_wallet.svg';
-import EmptyState from 'src/assets/images/empty-state-illustration.svg';
 import VaultIcon from 'src/assets/images/vault_icon.svg';
 import HideWalletIcon from 'src/assets/images/hide_wallet.svg';
 import ShowIcon from 'src/assets/images/show.svg';
@@ -388,7 +387,6 @@ function ManageWallets() {
           <Text color={`${colorMode}.secondaryText`} style={styles.emptySubText}>
             {settings.ManageWalletsEmptySubtitle}
           </Text>
-          <EmptyState />
         </Box>
       ) : (
         <FlatList
--- src/screens/SigningDevices/DeleteKeys.tsx
+++ src/screens/SigningDevices/DeleteKeys.tsx
@@ -10,7 +10,6 @@ import { SDIcons } from '../Vault/SigningDeviceIcons';
 import Text from 'src/components/KeeperText';
 import { getSignerDescription, getSignerNameFromType } from 'src/hardware';
 import DeleteIcon from 'src/assets/images/delete_bin.svg';
-import EmptyState from 'src/assets/images/empty-state-illustration.svg';
 import ShowIcon from 'src/assets/images/show.svg';
 import { useDispatch } from 'react-redux';
 import { updateSignerDetails } from 'src/store/sagaActions/wallets';
@@ -228,7 +227,6 @@ function DeleteKeys({ route }) {
             <Text color={`${colorMode}.secondaryText`} style={styles.emptySubText}>
               {signerText.clickToUnhide}
             </Text>
-            <EmptyState />
           </Box>
         ) : (
           <ScrollView
